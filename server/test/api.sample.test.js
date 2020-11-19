189197d74723fbb3da0928c0b996c79e86e20448
dimpl@JinYoung <dimple0416@naver.com>
2020-11-19 22:55:43 +0900

    [BE] TEST: api test 예제 코드 작성

---
+++ server/test/api.sample.test.js
@@ -0,0 +1,31 @@
+require('module-alias/register');
+const request = require('supertest');
+const app = require('@root/app');
+
+describe('login API test', () => {
+  const NAVER_URL = 'api/user/oauth/naver';
+  it('token check', done => {
+    // given
+    const expectedToken = 'Bearer tokenString';
+    const expectedStatus = 200;
+    const expectedMSG = 'ok';
+    try {
+      request(app)
+        .get(NAVER_URL) // when
+        .end((err, res) => {
+          if (err) {
+            throw err;
+          }
+          const { token, message } = res.body;
+          const { status } = res.headers;
+          expect(status).toBe(expectedStatus);
+          expect(token).toBe(expectedToken);
+          expect(message).toBe(expectedMSG);
+          done();
+        });
+      done();
+    } catch (err) {
+      done(err);
+    }
+  });
+});
