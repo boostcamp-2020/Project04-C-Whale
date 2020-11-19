5ea23d1a37b3444b6c92d9f9d73b3c162b99b52f
dimpl@JinYoung <dimple0416@naver.com>
2020-11-19 23:48:27 +0900

    [BE] TEST: api sample code 추가

--- server/test/api.sample.test.js
+++ server/test/api.sample.test.js
@@ -3,12 +3,14 @@ const request = require('supertest');
 const app = require('@root/app');
 
 describe('login API test', () => {
-  const NAVER_URL = 'api/user/oauth/naver';
+  //   const NAVER_URL = 'api/user/oauth/naver';
+  const NAVER_URL = '/';
   it('token check', done => {
     // given
     const expectedToken = 'Bearer tokenString';
     const expectedStatus = 200;
     const expectedMSG = 'ok';
+
     try {
       request(app)
         .get(NAVER_URL) // when
@@ -18,12 +20,12 @@ describe('login API test', () => {
           }
           const { token, message } = res.body;
           const { status } = res.headers;
+          // then
           expect(status).toBe(expectedStatus);
           expect(token).toBe(expectedToken);
           expect(message).toBe(expectedMSG);
           done();
         });
-      done();
     } catch (err) {
       done(err);
     }
