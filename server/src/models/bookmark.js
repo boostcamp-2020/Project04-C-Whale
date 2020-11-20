0104f8fad86ad589a548860abd67676c713880b3
dimpl@JinYoung <dimple0416@naver.com>
2020-11-20 11:14:22 +0900

    [BE] STYLE: sequelize 생성 시, db table name 단수형으로 설정

--- server/src/models/bookmark.js
+++ server/src/models/bookmark.js
@@ -1,15 +1,19 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = sequelize => {
-  sequelize.define('bookmark', {
-    id: {
-      primaryKey: true,
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
+  sequelize.define(
+    'bookmark',
+    {
+      id: {
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      url: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
     },
-    url: {
-      allowNull: false,
-      type: DataTypes.STRING,
-    },
-  });
+    { tableName: 'bookmark' },
+  );
 };
