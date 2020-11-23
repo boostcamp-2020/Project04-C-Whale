041dc96f2a1c1fe7dce6a5ca23d446bce097c410
woongs <ccw1021@naver.com>
2020-11-23 19:59:31 +0900

    [iOS] feat: worker에 getTasks() 추가
    - UI 테스트를 위해 현재 임시로 더미데이터를 넣어뒀습니다. API가 연동되고 추후 작업이 진행되면 메소드명부터 수정이 필요해보입니다.

--- iOS/HalgoraeDO/Sources/Scenes/TaskList/TaskListWorker.swift
+++ iOS/HalgoraeDO/Sources/Scenes/TaskList/TaskListWorker.swift
@@ -8,5 +8,15 @@
 import Foundation
 
 class TaskListWorker {
-    
+    func getTasks() -> [Task] {
+        return [
+            Task(title: "할고래두", subTasks: [
+                    Task(title: "hihi"),
+                    Task(title: "hihi2"),
+                    Task(title: "hehehe3"),
+                ]),
+            Task(title: "당연하지당연하지당연하지당연하지당연하지당연하지당연하지당연하지당연하지당연하지당연하지당연하지당연하지당연하지당연하지당연하지당연하지당연하지당연하지당연하지당연하지당연하지당연하지당연하지당연하지당연하지당연하지당연하지당연하지"),
+            Task(title: "두 말하면 섭함"),
+        ]
+    }
 }
