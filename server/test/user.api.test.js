88fcfd3c2466884ef15d56e127da9961518be021
dimpl@JinYoung <dimple0416@naver.com>
2020-11-23 20:22:46 +0900

    [BE] TEST: 기본 data 설정 및 label, user test 코드 작성

---
+++ server/test/user.api.test.js
@@ -0,0 +1,32 @@
+require('module-alias/register');
+const request = require('supertest');
+const app = require('@root/app');
+const expectedData = require('@test/expected-data');
+
+jest.mock('@models', () => {
+  // 추후 global setting 으로 한번에 설정하는 방안 생각
+  const connection = require('@test/sequelize-mock');
+  return connection;
+});
+
+describe('user api', () => {
+  it('users me', done => {
+    // given
+    const expectedUser = expectedData.users[0];
+
+    try {
+      request(app)
+        .get('/user/me') // when
+        .end((err, res) => {
+          if (err) {
+            throw err;
+          }
+          const user = res.body;
+          expect(user).toStrictEqual(expectedUser);
+          done();
+        });
+    } catch (err) {
+      done(err);
+    }
+  });
+});
