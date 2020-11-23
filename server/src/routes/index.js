6d0611624e993b5b74f824675e153d48d1325a29
dimpl@JinYoung <dimple0416@naver.com>
2020-11-23 20:24:25 +0900

    [BE] FEAT: label 생성 test code에 따른 label post api 구현

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -3,19 +3,31 @@ const express = require('express');
 const router = express.Router();
 
 const { models } = require('@models');
+const { responseHandler } = require('@utils/handler');
 
 router.get('/', (req, res, next) => {
   res.json();
 });
 
-router.get('/user', async (req, res) => {
-  const users = await models.user.findAll();
-  res.json(users);
+router.get('/user/me', async (req, res) => {
+  const user = { id: 'ff4dd832-1567-4d74-b41d-bd85e96ce329' };
+  const foundUser = await models.user.findOne(user.id);
+  delete user.dataValues.createdAt;
+  delete user.dataValues.updatedAt;
+  res.json(user);
 });
 
 router.get('/label', async (req, res) => {
   const labels = await models.label.findAll();
   res.json(labels);
+});
+
+router.post('/api/label', async (req, res) => {
+  const { id: userId } = req.user || { id: 'ff4dd832-1567-4d74-b41d-bd85e96ce329' };
+  const { color, title } = req.body;
+  const result = await models.label.create({ userId, color, title });
+
+  responseHandler(res, 200);
 });
 
 module.exports = router;
