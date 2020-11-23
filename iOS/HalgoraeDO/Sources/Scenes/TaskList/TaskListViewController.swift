93bcbd817c8f593c5203ac16846b51731d65a506
woongs <ccw1021@naver.com>
2020-11-23 19:55:25 +0900

    [iOS] feat: collectionView 레이아웃 및 데이터소스 구현
    - snapshot(taskItems:) 같은 경우 테스트를 위해서는 분리시켜야 할 것 같은데, snapshot까지도 테스트를 해야할지, 어떻게 테스트 해야할지 감이 오지않아 일단은 private 상태로 뒀습니다. 좀 더 학습 후 개선이 필요해보입니다.

--- iOS/HalgoraeDO/Sources/Scenes/TaskList/TaskListViewController.swift
+++ iOS/HalgoraeDO/Sources/Scenes/TaskList/TaskListViewController.swift
@@ -17,16 +17,21 @@ class TaskListViewController: UIViewController {
     
     var interactor: TaskListBusinessLogic?
     var router: (TaskListRoutingLogic & TaskListDataPassing)?
+    private var dataSource: UICollectionViewDiffableDataSource<String, Task>! = nil
     
     // MARK: View Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureLogic()
+        configureCollectionView()
+        configureDataSource()
     }
     
     // MARK: Initialize
+    // MARK: - Views
     
+    @IBOutlet weak private var taskListCollectionView: UICollectionView!
     private func configureLogic() {
         let presenter = TaskListPresenter(viewController: self)
         let interactor = TaskListInteractor(presenter: presenter, worker: TaskListWorker())
@@ -61,5 +66,52 @@ class TaskListViewController: UIViewController {
 }
 
 extension TaskListViewController: TaskListDisplayLogic {
+// MARK: - Configure CollectionView Layout
     
+private extension TaskListViewController {
+    private func configureCollectionView() {
+        taskListCollectionView.collectionViewLayout = generateLayout()
+}
+    
+    private func generateLayout() -> UICollectionViewLayout {
+        let listConfiguration = UICollectionLayoutListConfiguration(appearance: .sidebar)
+        let layout = UICollectionViewCompositionalLayout.list(using: listConfiguration)
+        return layout
+    }
+}
+
+// MARK: - Configure CollectionView Data Source
+
+private extension TaskListViewController {
+
+    private func configureDataSource() {
+        let cellRegistration = UICollectionView.CellRegistration<TaskCollectionViewListCell, Task> { (cell, indexPath, taskItem) in
+
+            var contentConfiguration = cell.defaultContentConfiguration()
+            contentConfiguration.text = taskItem.title
+            let disclosureOptions = UICellAccessory.OutlineDisclosureOptions(style: .header)
+            cell.accessories = taskItem.subTasks.isEmpty ? [] : [.outlineDisclosure(options: disclosureOptions)]
+            cell.contentConfiguration = contentConfiguration
+            cell.backgroundConfiguration = UIBackgroundConfiguration.clear()
+        }
+        
+        self.dataSource = UICollectionViewDiffableDataSource<String, Task>(collectionView: taskListCollectionView, cellProvider: { (collectionView, indexPath, task) -> UICollectionViewCell? in
+            
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: task)
+        })
+    }
+    
+    private func snapshot(taskItems: [Task]) -> NSDiffableDataSourceSectionSnapshot<Task> {
+        var snapshot = NSDiffableDataSourceSectionSnapshot<Task>()
+
+        func addItems(_ taskItems: [Task], to parent: Task?) {
+            snapshot.append(taskItems, to: parent)
+            for taskItem in taskItems where !taskItem.subTasks.isEmpty {
+                addItems(taskItem.subTasks, to: taskItem)
+            }
+        }
+
+        addItems(taskItems, to: nil)
+        return snapshot
+    }
 }
