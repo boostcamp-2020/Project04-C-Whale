354453018c467d7e257b1fa885869fc5fa34ce55
dimpl@JinYoung <dimple0416@naver.com>
2020-11-25 02:01:41 +0900

    [BE] TEST: expected data를 api 명세 return에 맞게 데이터 가공

--- server/test/label.api.test.js
+++ server/test/label.api.test.js
@@ -22,7 +22,19 @@ const UNAUTHORIZED_MSG = 'Unauthorized';
 describe('label api', () => {
   it('전체 label 조회 (token O)', done => {
     // given
-    const expectedLabels = seeder.labels;
+    const expectedLabels = seeder.labels.map(label => {
+      const copy = (() => {
+        const {
+          createdAt: deletedKey1,
+          updatedAt: deletedKey2,
+          userId: deletedKey3,
+          ...rest
+        } = label;
+        return rest;
+      })();
+      return copy;
+    });
+
     try {
       request(app)
         .get('/api/label') // when
@@ -34,7 +46,7 @@ describe('label api', () => {
           const { labels } = res.body;
           // then
           expect(res.status).toBe(SUCCESS_CODE);
-          expect(expectedLabels).toBe(labels);
+          expect(labels).toStrictEqual(expectedLabels);
           done();
         });
     } catch (err) {
@@ -50,7 +62,6 @@ describe('label api', () => {
           if (err) {
             throw err;
           }
-          console.log(res);
           // then
           expect(res.status).toBe(UNAUTHORIZED_CODE);
           expect(res.body.message).toBe(UNAUTHORIZED_MSG);
