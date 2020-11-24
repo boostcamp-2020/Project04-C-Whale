7ef8241cd3e7156dab8a64a7cb7a165a682c848d
Jin Young Park <jy2045@sogang.ac.kr>
2020-11-24 23:39:33 +0900

    [BE] FEAT: task 생성 API에 유효성검증 로직 추가

--- server/src/controllers/task.js
+++ server/src/controllers/task.js
@@ -1,12 +1,19 @@
 const { responseHandler } = require('@utils/handler');
 const sequelize = require('@models');
 const { models } = require('@models');
+const { isValidDueDate } = require('@utils/date');
 
 const createTask = async (req, res, next) => {
   const t = await sequelize.transaction();
 
   try {
     const { labelIdList, dueDate, ...rest } = req.body;
+
+    if (!isValidDueDate(dueDate)) {
+      const err = new Error('유효하지 않은 dueDate');
+      err.status = 400;
+      throw err;
+    }
 
     const newTask = await models.task.create({ dueDate, ...rest }, { transaction: t });
     await newTask.setLabels(JSON.parse(labelIdList), { transaction: t });
