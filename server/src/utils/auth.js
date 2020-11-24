8cdc28518de3890bc54060d9373749d1239f662a
dimpl@JinYoung <dimple0416@naver.com>
2020-11-24 18:29:36 +0900

    [BE] FEAT: jwt token 생성 및 naver authenticate 구현

--- server/src/utils/auth.js
+++ server/src/utils/auth.js
@@ -1,10 +1,15 @@
 const jwt = require('jsonwebtoken');
+const passport = require('passport');
 
 const TOKEN_HEADER = 'Bearer';
 
 const createJWT = user => {
   const { email, name, provider } = user;
-  return `${TOKEN_HEADER} ${jwt.sign({ email, name, provider }, process.env.JWT_SECRET)}`;
+  return `${TOKEN_HEADER} ${jwt.sign({ email, name, provider }, process.env.JWT_SECRET, {
+    expiresIn: '1h',
+  })}`;
 };
 
-module.exports = { createJWT };
+const passportNaverAuthenticate = passport.authenticate('naver', { session: false });
+
+module.exports = { createJWT, passportNaverAuthenticate };
