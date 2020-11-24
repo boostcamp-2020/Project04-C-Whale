ac97504856fb39d6701724bc3333efcf53839293
dimpl@JinYoung <dimple0416@naver.com>
2020-11-25 00:23:08 +0900

    [BE] REFACTOR: jwt token 생성시 user의 id 추가

--- server/src/utils/auth.js
+++ server/src/utils/auth.js
@@ -4,8 +4,7 @@ const passport = require('passport');
 const TOKEN_HEADER = 'Bearer';
 
 const createJWT = user => {
-  const { email, name, provider } = user;
-  return `${TOKEN_HEADER} ${jwt.sign({ email, name, provider }, process.env.JWT_SECRET, {
+  return `${TOKEN_HEADER} ${jwt.sign(user, process.env.JWT_SECRET, {
     expiresIn: '1h',
   })}`;
 };
