2f67f15bd0127d9c4f3245b73e57056ef9021022
Jin Young Park <jy2045@sogang.ac.kr>
2020-11-24 14:49:44 +0900

    [BE] REFACTOR: task controller try/catch로 리팩토링

--- server/src/controllers/task.js
+++ server/src/controllers/task.js
@@ -1,14 +1,18 @@
-const { responseHandler } = require('@utils/handler');
+const { responseHandler, errorHandler } = require('@utils/handler');
+const { isValidDueDate } = require('@utils/date');
 const taskService = require('@services/task');
 const labelService = require('@services/label');
 
-const createTask = async (req, res, next) => {
-  const { labelIdList, ...rest } = req.body;
+const createTask = async (req, res) => {
+  try {
+    const { labelIdList, dueDate, ...rest } = req.body;
 
-  const newTask = await taskService.createTask(rest);
-  await labelService.assignLabelToTask(newTask, labelIdList);
-
-  responseHandler(res, 201, { message: 'ok', id: newTask.id });
+    const newTask = await taskService.createTask(dueDate, rest);
+    await labelService.assignLabelToTask(newTask, labelIdList);
+    responseHandler(res, 201, { message: 'ok', id: newTask.id });
+  } catch (err) {
+    errorHandler(res, 400, { message: err.message });
+  }
 };
 
 module.exports = { createTask };
