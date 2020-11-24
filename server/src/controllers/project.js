9381b52a5ce6d729d4ceb16de7dac414db89985e
shkilo <kyle.hwan.k@gmail.com>
2020-11-25 02:28:01 +0900

    [BE] FEAT: project API 컨트롤러 구현
    + 프로젝트 개별 조회시 하위 task include
    + 프로젝트 생성시 하위 섹션 하나 생성하는 부분 transaction 이용
    + section 부분은 다른 파일로 분리하는것도 좋을것 같습니다

--- server/src/controllers/project.js
+++ server/src/controllers/project.js
@@ -37,4 +37,130 @@ const getProjects = async (req, res) => {
   }
 };
 
-module.exports = { getProjects };
+const getProjectById = async (req, res) => {
+  try {
+    const project = await models.project.findByPk(req.params.projectId, {
+      attributes: ['id', 'title', 'isList'],
+      include: {
+        model: models.section,
+        include: {
+          model: models.task,
+          include: {
+            model: models.task,
+            attributes: { exclude: ['createdAt', 'updatedAt'] },
+          },
+        },
+      },
+    });
+
+    responseHandler(res, 201, project);
+  } catch (err) {
+    errorHandler(res, 400, { message: err.message });
+  }
+};
+
+const createProject = async (req, res) => {
+  try {
+    await sequelize.transaction(async t => {
+      const project = await models.project.create(req.body, { transaction: t });
+      const section = await models.section.create({}, { transaction: t });
+      await section.setProject(project, { transaction: t });
+    });
+
+    responseHandler(res, 201, {
+      message: 'ok',
+    });
+  } catch (err) {
+    errorHandler(res, 400, { message: err.message });
+  }
+};
+
+const updateProject = async (req, res) => {
+  try {
+    await models.project.update(req.body, {
+      where: {
+        id: req.params.projectId,
+      },
+    });
+    responseHandler(res, 201, {
+      message: 'ok',
+    });
+  } catch (err) {
+    errorHandler(res, 400, { message: err.message });
+  }
+};
+
+const deleteProject = async (req, res) => {
+  try {
+    await models.project.destroy({
+      where: {
+        id: req.params.projectId,
+      },
+    });
+    responseHandler(res, 201, {
+      message: 'ok',
+    });
+  } catch (err) {
+    errorHandler(res, 400, { message: err.message });
+  }
+};
+
+const createSection = async (req, res) => {
+  try {
+    const project = await models.project.findByPk(req.params.projectId);
+
+    await sequelize.transaction(async t => {
+      const section = await models.section.create(req.body, { transaction: t });
+      await section.setProject(project, { transaction: t });
+    });
+
+    responseHandler(res, 201, {
+      message: 'ok',
+    });
+  } catch (err) {
+    errorHandler(res, 400, { message: err.message });
+  }
+};
+
+const updateSection = async (req, res) => {
+  try {
+    await models.section.update(req.body, {
+      where: {
+        id: req.params.sectionId,
+      },
+    });
+
+    responseHandler(res, 201, {
+      message: 'ok',
+    });
+  } catch (err) {
+    errorHandler(res, 400, { message: err.message });
+  }
+};
+
+const deleteSection = async (req, res) => {
+  try {
+    await models.project.destroy({
+      where: {
+        id: req.params.sectionId,
+      },
+    });
+
+    responseHandler(res, 201, {
+      message: 'ok',
+    });
+  } catch (err) {
+    errorHandler(res, 400, { message: err.message });
+  }
+};
+
+module.exports = {
+  getProjects,
+  getProjectById,
+  createProject,
+  updateProject,
+  deleteProject,
+  createSection,
+  updateSection,
+  deleteSection,
+};
