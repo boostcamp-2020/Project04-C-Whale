54e2a483077885807b6083e4239449b22113a4df
shkillo <kyle.hwan.k@gmail.com>
2020-11-25 18:19:55 +0900

    [BE] TEST: async 미들웨어를 try catch로 감싸주는 유틸함수 구현

--- server/src/controllers/task.js
+++ server/src/controllers/task.js
@@ -1,29 +1,24 @@
-const { responseHandler } = require('@utils/handler');
 const sequelize = require('@models');
 const { models } = require('@models');
+const { asyncTryCatch } = require('@utils/async-try-catch');
+const { responseHandler } = require('@utils/handler');
 const { isValidDueDate } = require('@utils/date');
 
-const createTask = async (req, res, next) => {
-  const t = await sequelize.transaction();
+const createTask = asyncTryCatch(async (req, res) => {
+  const { labelIdList, dueDate, ...rest } = req.body;
 
-  try {
-    const { labelIdList, dueDate, ...rest } = req.body;
+  if (!isValidDueDate(dueDate)) {
+    const err = new Error('유효하지 않은 dueDate');
+    err.status = 400;
+    throw err;
+  }
 
-    if (!isValidDueDate(dueDate)) {
-      const err = new Error('유효하지 않은 dueDate');
-      err.status = 400;
-      throw err;
-    }
-
+  await sequelize.transaction(async t => {
     const newTask = await models.task.create({ dueDate, ...rest }, { transaction: t });
     await newTask.setLabels(JSON.parse(labelIdList), { transaction: t });
-    await t.commit();
+  });
 
-    responseHandler(res, 201, { message: 'ok', id: newTask.id });
-  } catch (err) {
-    await t.rollback();
-    next(err);
-  }
-};
+  responseHandler(res, 201, { message: 'ok' });
+});
 
 module.exports = { createTask };
