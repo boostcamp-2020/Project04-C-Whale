7f003ceb775d7bed0a1886dd2b95053fdc301184
dimpl@JinYoung <dimple0416@naver.com>
2020-11-25 18:22:31 +0900

    [BE] FEAT: test code에 따라, valid check & auth check 후 label 생성 구현

--- server/src/controllers/label.js
+++ server/src/controllers/label.js
@@ -14,4 +14,35 @@ const getAllLabels = async (req, res, next) => {
   }
 };
 
-module.exports = { getAllLabels };
+const createLabel = async (req, res, next) => {
+  try {
+    const { title, color } = req.body;
+    const result = await labelModel.create({ title, color, userId: req.user.id });
+    if (result) {
+      responseHandler(res, 200);
+    } else {
+      throw Error('Internal Server Error');
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+const isValidPostDatas = async (req, res, next) => {
+  const error = new Error('Bad Request');
+  error.status = 400;
+  try {
+    const { title, color } = req.body;
+    if (!title && (typeof title !== 'string' || title === '')) {
+      throw error;
+    }
+    if (!color && (typeof color !== 'string' || color === '')) {
+      throw error;
+    }
+    next();
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { getAllLabels, createLabel, isValidPostDatas };
--- server/src/routes/label.js
+++ server/src/routes/label.js
@@ -4,12 +4,6 @@ const labelController = require('@controllers/label');
 
 router.get('/', authenticateUser, labelController.getAllLabels);
 
-// router.post('/', authenticateUser, async (req, res) => {
-//   const { id: userId } = req.user || { id: 'ff4dd832-1567-4d74-b41d-bd85e96ce329' };
-//   const { color, title } = req.body;
-//   const result = await models.label.create({ userId, color, title });
-
-//   responseHandler(res, 200);
-// });
+router.post('/', authenticateUser, labelController.isValidPostDatas, labelController.createLabel);
 
 module.exports = router;
