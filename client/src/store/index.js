48b17c12edb09fb9640f9a702e6f24d9c66b0658
Jin Young Park <jy2045@sogang.ac.kr>
2020-11-27 05:21:44 +0900

    [FE] FEAT: store 샘플코드 작성

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -4,8 +4,19 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    accessToken: null,
+  },
+  mutations: {
+    LOGIN(state, { accessToken }) {
+      state.accessToken = accessToken;
+      localStorage.setItem("token", accessToken);
+    },
+    LOGOUT(state) {
+      state.accessToken = null;
+      localStorage.removeItem("token");
+    },
+  },
   actions: {},
   modules: {},
 });
