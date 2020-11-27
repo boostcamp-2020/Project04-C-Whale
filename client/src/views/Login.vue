c58b317fa53358353d4670049fd4ff927242d777
Jin Young Park <jy2045@sogang.ac.kr>
2020-11-27 11:38:48 +0900

    [FE] FEAT: 네아로 URL 수정
    - .env에 추가

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -3,7 +3,7 @@
     <v-layout class="whaleGreen" fill-height d-flex align-center justify-center flex-column>
       <div class="login-layout white d-flex flex-column justify-space-around align-center py-10">
         <img width="200" src="../assets/halgoraedo.png" />
-        <a href="http://localhost:3000/api/user/oauth/naver">
+        <a :href="serverURL">
           <img width="200" src="../assets/naverlogin.png" />
         </a>
       </div>
@@ -14,6 +14,11 @@
 <script>
 export default {
   name: "Login",
+  data: function () {
+    return {
+      serverURL: process.env.VUE_APP_SERVER_URL + "/api/user/oauth/naver",
+    };
+  },
 };
 </script>
 
