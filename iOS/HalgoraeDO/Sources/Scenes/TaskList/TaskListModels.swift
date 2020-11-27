f4bf9fca5c28317313ecd265d617da902a6cffab
woongs <ccw1021@naver.com>
2020-11-28 01:00:10 +0900

    [iOS] feat: TaskViewModel 생성
    - 순서 재배치를 고려해서 position을 추가했습니다.

--- iOS/HalgoraeDO/Sources/Scenes/TaskList/TaskListModels.swift
+++ iOS/HalgoraeDO/Sources/Scenes/TaskList/TaskListModels.swift
@@ -5,7 +5,7 @@
 //  Created by woong on 2020/11/23.
 //
 
-import Foundation
+import UIKit
 
 enum TaskListModels {
     struct Request {
@@ -16,7 +16,49 @@ enum TaskListModels {
     
     }
     
-    struct ViewModel {
-    
+    struct TaskViewModel: Hashable {
+        var id: UUID
+        var title: String
+        var isCompleted: Bool
+        var tintColor: UIColor
+        var position: Int
+        var parentPosition: Int?
+        var subItems: [TaskViewModel]
+        
+        init(id: UUID,
+             title: String,
+             isCompleted: Bool = false,
+             tintColor: UIColor,
+             position: Int,
+             parentPosition: Int?,
+             subItems: [TaskViewModel]) {
+            self.id = id
+            self.title = title
+            self.isCompleted = isCompleted
+            self.tintColor = tintColor
+            self.position = position
+            self.parentPosition = parentPosition
+            self.subItems = subItems
+        }
+        
+        init(task: Task, position: Int, parentPosition: Int?) {
+            self.id = task.identifier
+            self.title = task.title
+            self.isCompleted = task.isCompleted
+            self.tintColor = task.priority.color
+            self.position = position
+            self.parentPosition = parentPosition
+            self.subItems = task.subTasks.enumerated().compactMap { (idx, task) in
+                TaskViewModel(task: task, position: idx, parentPosition: position)
+            }
+        }
+        
+        static func ==(lhs: Self, rhs: Self) -> Bool {
+            return lhs.id == rhs.id
+        }
+        
+        func hash(into hasher: inout Hasher) {
+            hasher.combine(id)
+        }
     }
 }
