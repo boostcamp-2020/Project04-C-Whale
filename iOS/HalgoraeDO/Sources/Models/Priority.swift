6a36307e7d7f6316b82ce397c650d4e1e59e1c0d
woongs <ccw1021@naver.com>
2020-11-28 01:08:46 +0900

    [iOS] feat: Priority에 color 추가

--- iOS/HalgoraeDO/Sources/Models/Priority.swift
+++ iOS/HalgoraeDO/Sources/Models/Priority.swift
@@ -21,6 +21,15 @@ enum Priority: Int, CaseIterable {
             case .four: return "우선순위 4"
         }
     }
+    
+    var color: UIColor {
+        switch self {
+            case .one: return .red
+            case .two: return .blue
+            case .three: return .orange
+            case .four: return .black
+        }
+    }
 }
 
 // MARK: - For PopoverViewModel
@@ -35,10 +44,10 @@ extension Priority {
     var viewModel: ViewModel {
         let image = UIImage(systemName: "flag.fill")?.scaled(to: .init(width: 30, height: 30))
         switch self {
-            case .one: return ViewModel(title: title, tintColor: .red, image: image)
-            case .two: return ViewModel(title: title, tintColor: .blue, image: image)
-            case .three: return ViewModel(title: title, tintColor: .orange, image: image)
-            case .four: return ViewModel(title: title, tintColor: .black, image: image)
+            case .one: return ViewModel(title: title, tintColor: color, image: image)
+            case .two: return ViewModel(title: title, tintColor: color, image: image)
+            case .three: return ViewModel(title: title, tintColor: color, image: image)
+            case .four: return ViewModel(title: title, tintColor: color, image: image)
         }
     }
 }
