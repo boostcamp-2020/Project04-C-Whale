8f01042a214c96c8486a52119ec107e5fe08a457
shkilo <kyle.hwan.k@gmail.com>
2020-11-30 21:53:36 +0900

    [FE] TEST: project 페이지 임시 라우팅

--- client/src/App.vue
+++ client/src/App.vue
@@ -14,10 +14,12 @@ export default {
     const accessToken = location.search.split("token=")[1];
     if (accessToken) {
       localStorage.setItem("token", accessToken);
+      // this.$store.commit('LOGIN', accessToken);
       location.href = "/";
     }
     if (localStorage.getItem("token")) {
-      router.push("/today").catch(() => {});
+      // router.push("/today").catch(() => {});
+      router.push("/project/b7f253e5-7b6b-4ee2-b94e-369ffcdffb5f").catch(() => {});
     } else {
       router.push("/login").catch(() => {});
     }
