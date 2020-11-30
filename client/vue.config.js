0a3ba95d332b82084788f977d938766f8801e813
Jin Young Park <jy2045@sogang.ac.kr>
2020-11-30 17:22:43 +0900

    [FE] FIX: eslint-loader 비활성화

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,9 +1,4 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
-  chainWebpack: (config) => {
-    config.module.rule('eslint').use('eslint-loader').tap((options) => {
-      options.fix = true;
-      return options;
-    })
-  }
+  lintOnSave: false
 };
