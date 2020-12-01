280bcc0ba02ad7f2398f166ccdb4b92f22d51029
Jin Young Park <jy2045@sogang.ac.kr>
2020-12-02 04:19:14 +0900

    [FE] FEAT: project store 추가 구현
    - getProjects 관련 action, mutation, state 구현

--- client/src/store/project.js
+++ client/src/store/project.js
@@ -8,10 +8,13 @@ const state = {
     isList: null,
     sections: [],
   },
+  projectInfos: [],
 };
 
 const getters = {
   currentProject: (state) => state.currentProject,
+  projectInfos: (state) => state.projectInfos,
+  managedProject: (state) => state.projects.filter((project) => project.title === "관리함"),
 };
 
 const actions = {
@@ -37,10 +40,21 @@ const actions = {
       alert("프로젝트 조회 요청 실패");
     }
   },
+  async fetchProjectInfos({ commit }) {
+    try {
+      const { data: projects } = await projectAPI.getProjects();
+
+      commit("setProjects", projects);
+    } catch (err) {
+      alert("프로젝트 전체 정보 조회 요청 실패");
+    }
+  },
 };
 
 const mutations = {
+  //TODO: function vs arrow-function style-guide 보고 통일하기
   setCurrentProject: (state, currentProject) => (state.currentProject = currentProject),
+  setProjects: (state, projects) => (state.projects = projects),
   // newTodo: (state, todo) => state.todos.unshift(todo),
 };
 
