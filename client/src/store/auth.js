6728d128baff24656a2c8f333627dd26c1a1f18c
Jin Young Park <jy2045@sogang.ac.kr>
2020-12-01 17:50:46 +0900

    [FE] FEAT: auth store 구현
    - SET_TOKEN, SET_USER, LOGOUT의 mutation 구현
    - checkuser 비동기 로직 구현

--- client/src/store/auth.js
+++ client/src/store/auth.js
@@ -1,18 +1,56 @@
+import router from "@/router/index.js";
+import userAPI from "@/api/user.js";
+
 const state = {
-  accessToken: null,
-};
-const mutations = {
-  LOGIN(state, { accessToken }) {
-    state.accessToken = accessToken;
-    localStorage.setItem("token", accessToken);
-  },
-  LOGOUT(state) {
-    state.accessToken = null;
-    localStorage.removeItem("token");
+  user: {
+    id: "",
+    name: "",
+    email: "",
   },
 };
 
-const actions = {};
+const mutations = {
+  SET_TOKEN(state, accessToken) {
+    localStorage.setItem("token", accessToken);
+    location.replace("/");
+  },
+  SET_USER(state, user) {
+    state.user = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    };
+  },
+  LOGOUT() {
+    localStorage.removeItem("token");
+    location.replace("/");
+  },
+};
+
+const actions = {
+  setToken({ commit }, { accessToken }) {
+    commit("SET_TOKEN", accessToken);
+  },
+
+  async checkUser({ commit }) {
+    try {
+      const { data: user } = await userAPI.authorize();
+      commit("SET_USER", user);
+
+      if (location.pathname === "/") {
+        router.replace("/today");
+      } else {
+        router.replace(location.pathname);
+      }
+    } catch (err) {
+      alert("로그인에 실패했습니다");
+    }
+  },
+
+  logout({ commit }) {
+    commit("LOGOUT");
+  },
+};
 
 export default {
   state,
