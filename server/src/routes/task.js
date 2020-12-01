d952cbc32e63dfb814556b8f6e68efba654a6752
dimpl@JinYoung <dimple0416@naver.com>
2020-12-01 18:00:01 +0900

    [BE] REFACTOR: task api router에 인증 미들웨어 추가

--- server/src/routes/task.js
+++ server/src/routes/task.js
@@ -1,14 +1,16 @@
 const router = require('express').Router();
 const taskController = require('@controllers/task');
+const { authenticateUser } = require('@utils/auth');
 
-router.get('/:taskId', taskController.getTaskById);
-router.post('/', taskController.createTask);
-router.patch('/:taskId', taskController.updateTask);
-router.delete('/:taskId', taskController.deleteTask);
+// router.get('/', )
+router.get('/:taskId', authenticateUser, taskController.getTaskById);
+router.post('/', authenticateUser, taskController.createTask);
+router.patch('/:taskId', authenticateUser, taskController.updateTask);
+router.delete('/:taskId', authenticateUser, taskController.deleteTask);
 
-router.get('/:taskId/comment', taskController.getComments);
-router.post('/:taskId/comment', taskController.createComment);
-router.put('/:taskId/comment/:commentId', taskController.updateComment);
-router.delete('/:taskId/comment/:commentId', taskController.deleteComment);
+router.get('/:taskId/comment', authenticateUser, taskController.getComments);
+router.post('/:taskId/comment', authenticateUser, taskController.createComment);
+router.put('/:taskId/comment/:commentId', authenticateUser, taskController.updateComment);
+router.delete('/:taskId/comment/:commentId', authenticateUser, taskController.deleteComment);
 
 module.exports = router;
