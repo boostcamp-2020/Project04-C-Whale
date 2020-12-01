7859001825a8f3333c7471bdf42e02af983aebdc
dimpl@JinYoung <dimple0416@naver.com>
2020-12-02 01:27:44 +0900

    [FE] REFACTOR: 검색 API에 따른 로직 수정

--- client/src/components/task/Search.vue
+++ client/src/components/task/Search.vue
@@ -10,7 +10,7 @@
           color="green"
           hide-no-data
           hide-selected
-          item-text="Description"
+          item-text="title"
           item-value="API"
           placeholder="검색"
           prepend-icon="mdi-magnify"
@@ -18,12 +18,6 @@
           solo
         ></v-autocomplete>
       </v-card-text>
-      <!-- <v-text-field
-        @click="show = !show"
-        appendIcon="mdi-magnify"
-        placeholder="검색"
-        solo
-      ></v-text-field> -->
     </v-flex>
     <v-container v-if="show">
       <v-list v-for="task in lastSearched" :key="task.id">{{ task.title }}</v-list>
@@ -33,17 +27,14 @@
 
 <script>
 import router from "@/router/index.js";
+import taskAPI from "@/api/task.js";
 
 export default {
   data() {
     return {
       show: false,
-      lastSearched: [
-        { id: "1212sdkf-124oij", title: "hi" },
-        { id: "12asd-1234", title: "test" },
-      ],
-      descriptionLimit: 60,
-      entries: [],
+      titleLimit: 60,
+      tasks: [],
       isLoading: false,
       model: null,
       search: null,
@@ -61,24 +52,22 @@ export default {
       });
     },
     items() {
-      return this.entries.map((entry) => {
-        // TODO : API 연동 후, 변수명 title로 변경, entry에서도 title로 바꿔야한다.
-        const Description =
-          entry.Description.length > this.descriptionLimit
-            ? entry.Description.slice(0, this.descriptionLimit) + "..."
-            : entry.Description; // display length 설정
+      return this.tasks.map((task) => {
+        const title =
+          task.title.length > this.titleLimit
+            ? task.title.slice(0, this.titleLimit) + "..."
+            : task.title; // display length 설정
 
-        return { Description, ...entry };
+        return { title, ...task };
       });
     },
   },
 
   watch: {
     model(obj) {
-      //TODO API 연동 후, obj.id로 바뀌어야한다.
-      router.push(`/task/${obj.Description}`).catch(() => {});
+      router.push(`/task/${obj.id}`).catch(() => {});
     },
-    search() {
+    async search() {
       // Items have already been loaded
       // or Items have already been requested
       if (this.items.length > 0 || this.isLoading) {
@@ -88,18 +77,10 @@ export default {
       this.isLoading = true;
 
       // Lazily load input items,
-      // TODO 여기에 serach 받아와서 표현 & axios로 바꿔야함
-      fetch("https://api.publicapis.org/entries")
-        .then((res) => res.json())
-        .then((res) => {
-          const { count, entries } = res;
-          this.count = count;
-          this.entries = entries;
-        })
-        .catch((err) => {
-          console.log(err);
-        })
-        .finally(() => (this.isLoading = false));
+      const res = await taskAPI.serachTask();
+
+      this.tasks = res.data.tasks;
+      this.isLoading = false;
     },
   },
 };
