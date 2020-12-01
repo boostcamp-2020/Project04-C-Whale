8a61c71ff77730cae054d5e177da06c515c90c3f
Jin Young Park <jy2045@sogang.ac.kr>
2020-12-02 02:16:23 +0900

    [FE] FEAT: FavoriteProject, LeftMenu 컴포넌트 틀 잡기

---
+++ client/src/components/menu/FavoriteProjectList.vue
@@ -0,0 +1,11 @@
+<template>
+  <ul>
+    <li><span></span><span>관리함</span></li>
+    <li><span></span><span>오늘</span></li>
+    <li><span></span><span>다음</span></li>
+  </ul>
+</template>
+
+<script>
+export default {};
+</script>
---
+++ client/src/components/menu/LeftMenu.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <favorite-project-list></favorite-project-list>
+    <v-list-group :value="true" no-action sub-group>
+      <template v-slot:activator>
+        <v-list-item-content>
+          <v-list-item-title>프로젝트</v-list-item-title>
+        </v-list-item-content>
+      </template>
+    </v-list-group>
+  </div>
+</template>
+
+<script>
+import FavoriteProjectList from "./FavoriteProjectList";
+
+export default {
+  components: {
+    "favorite-project-list": FavoriteProjectList,
+  },
+};
+</script>
