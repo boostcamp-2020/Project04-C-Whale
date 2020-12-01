7137fd36124a3444ea33ff94c937e5d43b732afb
Jin Young Park <jy2045@sogang.ac.kr>
2020-12-02 04:17:38 +0900

    [FE] FEAT: projectAPI getProjects 추가

--- client/src/components/menu/LeftMenu.vue
+++ client/src/components/menu/LeftMenu.vue
@@ -7,16 +7,29 @@
           <v-list-item-title>프로젝트</v-list-item-title>
         </v-list-item-content>
       </template>
+      <v-list-item v-for="project in projectInfos" :key="project.id">
+        <v-list-item-content>
+          <v-list-item-title>{{ project.title }} {{ project.taskCount }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
     </v-list-group>
   </div>
 </template>
 
 <script>
 import FavoriteProjectList from "./FavoriteProjectList";
+import { mapGetters, mapActions } from "vuex";
 
 export default {
   components: {
     "favorite-project-list": FavoriteProjectList,
+  },
+  computed: mapGetters(["projectInfos"]),
+  methods: {
+    ...mapActions(["fetchProjectInfos"]),
+  },
+  created() {
+    this.fetchProjectInfos();
   },
 };
 </script>
