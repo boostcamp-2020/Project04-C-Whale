3f7b9729e84841e438a3697bf3da2deacddd9b95
dimpl@JinYoung <dimple0416@naver.com>
2020-12-01 21:34:57 +0900

    [BE] TEST: 전체 task 조회를 위한 seed 추가

--- server/test/test-seed.js
+++ server/test/test-seed.js
@@ -17,6 +17,14 @@ const users = [
     createdAt: new Date(),
     updatedAt: new Date(),
   },
+  {
+    id: 'c83abea3-bc93-44a4-b56c-dda6c742a94d',
+    email: 'kyle@naver.com',
+    name: 'kyle',
+    provider: 'naver',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
 ];
 
 const priorities = [
@@ -58,7 +66,7 @@ const projects = [
   },
   {
     id: 'f7605077-96ec-4365-88fc-a9c3af4a084e',
-    creatorId: 'ff4dd832-1567-4d74-b41d-bd85e96ce329',
+    creatorId: users[1].id,
     title: '프로젝트 2',
     isList: false,
     isFavorite: true,
@@ -71,6 +79,12 @@ const sections = [
   {
     id: '7abf0633-bce2-4972-9249-69f287db8a47',
     projectId: projects[0].id,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    id: 'efd8be46-ebc7-438d-931a-036a4b28789f',
+    projectId: projects[1].id,
     createdAt: new Date(),
     updatedAt: new Date(),
   },
@@ -131,6 +145,18 @@ const tasks = [
     parentId: '13502adf-83dd-4e8e-9acf-5c5a0abd5b1b',
     title: '작업 5',
     dueDate: new Date('2020-10-24T14:23:24.090Z'),
+    position: 1,
+    isDone: false,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    id: '568d0233-0e6e-4ff2-bdae-4c492e55f111',
+    projectId: projects[1].id,
+    sectionId: sections[1].id,
+    parentId: null,
+    title: '작업 6',
+    dueDate: new Date(),
     position: 1,
     isDone: false,
     createdAt: new Date(),
