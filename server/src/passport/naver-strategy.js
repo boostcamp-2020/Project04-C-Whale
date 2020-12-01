1ead44bec6c414dec91b32fb6388852fe7c62dcc
dimpl@JinYoung <dimple0416@naver.com>
2020-12-01 21:03:39 +0900

    [BE] REFACTOR: passport에서 model 접근 로직을 service로 수정

--- server/src/passport/naver-strategy.js
+++ server/src/passport/naver-strategy.js
@@ -1,6 +1,5 @@
 const NaverStrategy = require('passport-naver').Strategy;
-// const userService = require('@services/user');
-const userModel = require('@models').models.user;
+const userService = require('@services/user');
 
 const data = {
   clientID: process.env.NAVER_CLIENT_ID,
@@ -12,13 +11,7 @@ const getNaverUser = async (accessToken, refreshToken, profile, done) => {
   const NAVER = 'naver';
   try {
     const { email, nickname } = profile._json;
-
-    // let user = await userService.retrieveByEmail(email, NAVER);
-    const [user] = await userModel.findOrCreate({
-      where: { email, provider: NAVER },
-      attributes: ['id', 'email', 'name', 'provider'],
-      defaults: { email, name: nickname, provider: NAVER },
-    });
+    const [user] = await userService.retrieveOrCreate(email, nickname, NAVER);
 
     return done(null, user.toJSON());
   } catch (err) {
