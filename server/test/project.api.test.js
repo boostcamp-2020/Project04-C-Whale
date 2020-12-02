bf38037ca83fb031a457d7164a8e925f999a2fd0
dimpl@JinYoung <dimple0416@naver.com>
2020-12-02 14:38:51 +0900

    [BE] REFACTOR: project API test code 리팩토링

--- server/test/project.api.test.js
+++ server/test/project.api.test.js
@@ -2,6 +2,8 @@ require('module-alias/register');
 const request = require('supertest');
 const app = require('@root/app');
 const seeder = require('@test/test-seed');
+const status = require('@test/response-status');
+const { createJWT } = require('@utils/auth');
 
 beforeAll(async done => {
   await seeder.up();
@@ -13,258 +15,226 @@ afterAll(async done => {
   done();
 });
 
-const SUCCESS_CODE = 201;
-const SUCCESS_MSG = 'ok';
-
 describe('get all projects', () => {
-  it('project get all 일반', done => {
+  it('project get all 일반', async done => {
+    // given
+    const expectedUser = seeder.users[0];
     const expectedProjects = seeder.projects.map(project => {
-      const tasks = seeder.tasks.filter(task => task.projectId === project.id);
+      const tasks = seeder.tasks.filter(
+        task => project.creatorId === expectedUser.id && task.projectId === project.id,
+      );
       const { id, title } = project;
       return { id, title, taskCount: tasks.length };
     });
-    //  [
-    //   { id: 'b7f253e5-7b6b-4ee2-b94e-369ffcdffb5f', taskCount: 5, title: '프로젝트 1' },
-    //   { id: 'f7605077-96ec-4365-88fc-a9c3af4a084e', taskCount: 0, title: '프로젝트 2' },
-    //   { taskCount: 2, title: '오늘' },
-    // ];
 
-    try {
-      request(app)
-        .get('/api/project')
-        .end((err, res) => {
-          if (err) {
-            throw err;
-          }
-          expect(
-            res.body.every(project =>
-              expectedProjects.some(
-                expectedProject =>
-                  Object.entries(project).toString === Object.entries(expectedProject).toString,
-              ),
-            ),
-          ).toBeTruthy();
+    // when
+    const res = await request(app)
+      .get('/api/project')
+      .set('Authorization', `Bearer ${createJWT(expectedUser)}`);
+    const recievedProjects = res.body;
 
-          done();
-        });
-    } catch (err) {
-      done(err);
-    }
+    // then
+    expect(
+      recievedProjects.every(project =>
+        expectedProjects.some(
+          expectedProject =>
+            Object.entries(project).toString === Object.entries(expectedProject).toString,
+        ),
+      ),
+    ).toBeTruthy();
+
+    done();
   });
 });
 
 describe('get project by id', () => {
-  it('project get by id 일반', done => {
-    const expectedChildTaskId = '8d62f93c-9233-46a9-a5cf-ec18ad5a36f4';
+  it('project get by id 일반', async done => {
+    // given
+    const expectedChildTaskId = seeder.tasks[3].id;
+    const expectedProjectId = seeder.projects[0].id;
+    const expectedUser = seeder.users[0];
 
-    try {
-      request(app)
-        .get('/api/project/b7f253e5-7b6b-4ee2-b94e-369ffcdffb5f')
-        .end((err, res) => {
-          if (err) {
-            throw err;
-          }
-          const childTask = res.body.sections[0].tasks[0].tasks[0];
-          expect(childTask.id).toEqual(expectedChildTaskId);
-          done();
-        });
-    } catch (err) {
-      done(err);
-    }
+    // when
+    const res = await request(app)
+      .get(`/api/project/${expectedProjectId}`)
+      .set('Authorization', `Bearer ${createJWT(expectedUser)}`);
+
+    const childTask = res.body.sections[0].tasks[0].tasks[0];
+
+    // then
+    expect(childTask.id).toEqual(expectedChildTaskId);
+    done();
   });
 });
 
 describe('create project', () => {
-  it('create project 일반', done => {
+  it('create project 일반', async done => {
+    // given
     const requestBody = {
       title: '새 프로젝트',
       isList: true,
     };
+    const expectedUser = seeder.users[0];
 
-    try {
-      request(app)
-        .post('/api/project/')
-        .send(requestBody)
-        .end((err, res) => {
-          if (err) {
-            throw err;
-          }
-          expect(res.status).toBe(SUCCESS_CODE);
-          expect(res.body.message).toBe(SUCCESS_MSG);
-          done();
-        });
-    } catch (err) {
-      done(err);
-    }
+    // when
+    const res = await request(app)
+      .post('/api/project/')
+      .set('Authorization', `Bearer ${createJWT(expectedUser)}`)
+      .send(requestBody);
+
+    // then
+    expect(res.status).toBe(status.SUCCESS.POST.CODE);
+    expect(res.body.message).toBe(status.SUCCESS.MSG);
+    done();
   });
 });
 
 describe('update project', () => {
-  it('update project PUT', done => {
+  it('update project PUT', async done => {
+    // given
+    const expectedProjectId = seeder.projects[0].id;
     const requestBody = {
       title: '변경된 프로젝트',
       isList: true,
       isFavorite: true,
     };
+    const expectedUser = seeder.users[0];
 
-    try {
-      request(app)
-        .put('/api/project/b7f253e5-7b6b-4ee2-b94e-369ffcdffb5f')
-        .send(requestBody)
-        .end((err, res) => {
-          if (err) {
-            throw err;
-          }
-          expect(res.status).toBe(SUCCESS_CODE);
-          expect(res.body.message).toBe(SUCCESS_MSG);
-          done();
-        });
-    } catch (err) {
-      done(err);
-    }
+    // when
+    const res = await request(app)
+      .put(`/api/project/${expectedProjectId}`)
+      .set('Authorization', `Bearer ${createJWT(expectedUser)}`)
+      .send(requestBody);
+
+    // then
+    expect(res.status).toBe(status.SUCCESS.CODE);
+    expect(res.body.message).toBe(status.SUCCESS.MSG);
+    done();
   });
 
-  it('update project PATCH', done => {
+  it('update project PATCH', async done => {
+    // given
+    const expectedUser = seeder.users[0];
+    const expectedProjectId = seeder.projects[0].id;
     const requestBody = {
       title: '변경된 프로젝트',
       isList: true,
       isFavorite: true,
     };
 
-    try {
-      request(app)
-        .patch('/api/project/b7f253e5-7b6b-4ee2-b94e-369ffcdffb5f')
-        .send(requestBody)
-        .end((err, res) => {
-          if (err) {
-            throw err;
-          }
-          expect(res.status).toBe(SUCCESS_CODE);
-          expect(res.body.message).toBe(SUCCESS_MSG);
-          done();
-        });
-    } catch (err) {
-      done(err);
-    }
+    // when
+    const res = await request(app)
+      .patch(`/api/project/${expectedProjectId}`)
+      .set('Authorization', `Bearer ${createJWT(expectedUser)}`)
+      .send(requestBody);
+
+    // then
+    expect(res.status).toBe(status.SUCCESS.CODE);
+    expect(res.body.message).toBe(status.SUCCESS.MSG);
+    done();
   });
 });
 
 describe('delete project', () => {
-  it('delete project 일반', done => {
-    try {
-      request(app)
-        .delete('/api/project/f7605077-96ec-4365-88fc-a9c3af4a084e')
-        .end((err, res) => {
-          if (err) {
-            throw err;
-          }
-          expect(res.status).toBe(SUCCESS_CODE);
-          expect(res.body.message).toBe(SUCCESS_MSG);
-          done();
-        });
-    } catch (err) {
-      done(err);
-    }
+  it('delete project 일반', async done => {
+    // given
+    const expectedUser = seeder.users[0];
+    const expectedProjectId = seeder.projects[1];
+    // when
+    const res = await request(app)
+      .delete(`/api/project/${expectedProjectId}`)
+      .set('Authorization', `Bearer ${createJWT(expectedUser)}`);
+
+    // then
+    expect(res.status).toBe(status.SUCCESS.CODE);
+    expect(res.body.message).toBe(status.SUCCESS.MSG);
+    done();
   });
 });
 
 describe('create section', () => {
-  it('create project 일반', done => {
+  it('create project 일반', async done => {
+    // given
+    const expectedUser = seeder.users[0];
+    const expectedProjectId = seeder.projects[0].id;
     const requestBody = {
       title: '새로운 섹션',
     };
 
-    try {
-      request(app)
-        .post('/api/project/b7f253e5-7b6b-4ee2-b94e-369ffcdffb5f/section')
-        .send(requestBody)
-        .end((err, res) => {
-          if (err) {
-            throw err;
-          }
-          expect(res.status).toBe(SUCCESS_CODE);
-          expect(res.body.message).toBe(SUCCESS_MSG);
-          done();
-        });
-    } catch (err) {
-      done(err);
-    }
+    // when
+    const res = await request(app)
+      .post(`/api/project/${expectedProjectId}/section`)
+      .set('Authorization', `Bearer ${createJWT(expectedUser)}`)
+      .send(requestBody);
+
+    // then
+    expect(res.status).toBe(status.SUCCESS.POST.CODE);
+    expect(res.body.message).toBe(status.SUCCESS.MSG);
+    done();
   });
 });
 
 describe('update section task positions', () => {
-  it('update section task positions 일반', done => {
+  it('update section task positions 일반', async done => {
+    // given
+    const expectedUser = seeder.users[0];
+    const expectedProjectId = seeder.projects[0].id;
+    const expectedSectionId = seeder.sections[0].id;
     const requestBody = {
-      orderedTasks: [
-        '7d62f93c-9233-46a9-a5cf-ec18ad5a36f4',
-        'cd62f93c-9233-46a9-a5cf-ec18ad5a36f4',
-        '13502adf-83dd-4e8e-9acf-5c5a0abd5b1b',
-      ],
+      orderedTasks: [seeder.tasks[2].id, seeder.tasks[1].id, seeder.tasks[0].id],
     };
 
-    try {
-      request(app)
-        .post(
-          '/api/project/b7f253e5-7b6b-4ee2-b94e-369ffcdffb5f/section/7abf0633-bce2-4972-9249-69f287db8a47/task',
-        )
-        .send(requestBody)
-        .end((err, res) => {
-          if (err) {
-            throw err;
-          }
-          expect(res.status).toBe(SUCCESS_CODE);
-          expect(res.body.message).toBe(SUCCESS_MSG);
-          done();
-        });
-    } catch (err) {
-      done(err);
-    }
+    // when
+    const res = await request(app)
+      .post(`/api/project/${expectedProjectId}/section/${expectedSectionId}/task`)
+      .set('Authorization', `Bearer ${createJWT(expectedUser)}`)
+      .send(requestBody);
+
+    // then
+    expect(res.status).toBe(status.SUCCESS.CODE);
+    expect(res.body.message).toBe(status.SUCCESS.MSG);
+    done();
   });
 });
 
 describe('update section', () => {
-  it('update section 일반', done => {
+  it('update section 일반', async done => {
+    // given
+    const expectedUser = seeder.users[0];
+    const expectedProjectId = seeder.projects[0].id;
+    const expectedSectionId = seeder.sections[0].id;
     const requestBody = {
       title: '바뀐 섹션',
     };
 
-    try {
-      request(app)
-        .put(
-          '/api/project/b7f253e5-7b6b-4ee2-b94e-369ffcdffb5f/section/7abf0633-bce2-4972-9249-69f287db8a47',
-        )
-        .send(requestBody)
-        .end((err, res) => {
-          if (err) {
-            throw err;
-          }
-          expect(res.status).toBe(SUCCESS_CODE);
-          expect(res.body.message).toBe(SUCCESS_MSG);
-          done();
-        });
-    } catch (err) {
-      done(err);
-    }
+    // when
+    const res = await request(app)
+      .put(`/api/project/${expectedProjectId}/section/${expectedSectionId}`)
+      .set('Authorization', `Bearer ${createJWT(expectedUser)}`)
+      .send(requestBody);
+
+    // then
+    expect(res.status).toBe(status.SUCCESS.CODE);
+    expect(res.body.message).toBe(status.SUCCESS.MSG);
+    done();
   });
 });
 
 describe('delete section', () => {
-  it('update section 일반', done => {
-    try {
-      request(app)
-        .delete(
-          '/api/project/b7f253e5-7b6b-4ee2-b94e-369ffcdffb5f/section/7abf0633-bce2-4972-9249-69f287db8a47',
-        )
-        .end((err, res) => {
-          if (err) {
-            throw err;
-          }
-          expect(res.status).toBe(SUCCESS_CODE);
-          expect(res.body.message).toBe(SUCCESS_MSG);
-          done();
-        });
-    } catch (err) {
-      done(err);
-    }
+  it('update section 일반', async done => {
+    // given
+    const expectedUser = seeder.users[0];
+    const expectedProjectId = seeder.projects[0].id;
+    const expectedSectionId = seeder.sections[0].id;
+
+    // when
+    const res = await request(app)
+      .delete(`/api/project/${expectedProjectId}/section/${expectedSectionId}`)
+      .set('Authorization', `Bearer ${createJWT(expectedUser)}`);
+
+    // then
+    expect(res.status).toBe(status.SUCCESS.CODE);
+    expect(res.body.message).toBe(status.SUCCESS.MSG);
+    done();
   });
 });
