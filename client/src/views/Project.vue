3b107ef7cd8993c7010e644d1a7dcaa99830448e
Jin Young Park <jy2045@sogang.ac.kr>
2020-12-03 04:38:40 +0900

    [FE] REFACTOR: project Container 내부 데이터 props로 변경

--- client/src/views/Project.vue
+++ client/src/views/Project.vue
@@ -1,11 +1,22 @@
 <template>
-  <project-container />
+  <project-container :project="currentProject" />
 </template>
 
 <script>
+import { mapGetters, mapActions } from "vuex";
 import ProjectContainer from "../components/project/ProjectContainer";
 
 export default {
   components: { ProjectContainer },
+  methods: {
+    ...mapActions(["fetchCurrentProject"]),
+  },
+  computed: mapGetters(["currentProject"]),
+  created() {
+    this.fetchCurrentProject(this.$route.params.projectId);
+  },
+  beforeRouteUpdate(to, from, next) {
+    this.fetchCurrentProject(to.params.projectId);
+  },
 };
 </script>
