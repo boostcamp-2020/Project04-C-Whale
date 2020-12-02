8cf2a5e1c9503f3e11945ab197f153b2e60c9796
woongs <ccw1021@naver.com>
2020-12-02 18:26:40 +0900

    [iOS] feat: BorderRadiusButton 추가

---
+++ iOS/HalgoraeDO/Sources/Views/BorderRadiusButton.swift
@@ -0,0 +1,41 @@
+//
+//  BorderButton.swift
+//  HalgoraeDO
+//
+//  Created by woong on 2020/12/02.
+//
+
+import UIKit
+
+class BorderRadiusButton: UIButton {
+    
+    @IBInspectable
+    var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderColor: UIColor {
+        get {
+            return UIColor(cgColor: layer.borderColor!)
+        }
+        set {
+            layer.borderColor = newValue.cgColor
+        }
+    }
+    
+    @IBInspectable
+    var radius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+}
