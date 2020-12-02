91d53bdd0ade4634bf4e881bdf1ead3f2a06eed1
dimpl@JinYoung <dimple0416@naver.com>
2020-12-03 01:51:51 +0900

    [FE] FEAT: 작업 클릭시 라우터 이동 및 모달 표출 구현

--- client/src/components/project/TaskItem.vue
+++ client/src/components/project/TaskItem.vue
@@ -8,18 +8,39 @@
       ></v-radio>
     </v-list-item-action>
 
-    <v-list-item-content>
-      <v-list-item-title>{{ task.title }}</v-list-item-title>
-    </v-list-item-content>
+    <div class="task-div" @click="showModal({ taskId: task.id })">
+      <v-list-item-content>
+        <v-list-item-title>{{ task.title }}</v-list-item-title>
+      </v-list-item-content>
+    </div>
+
+    <v-dialog v-model="dialog" max-width="290" @click:outside="hideModal()">
+      <router-view />
+    </v-dialog>
   </v-list-item>
 </template>
 
 <script>
 import { mapActions } from "vuex";
+import router from "@/router";
 
 export default {
+  data() {
+    return {
+      dialog: false,
+    };
+  },
   methods: {
     ...mapActions(["updateTaskToDone"]),
+    showModal(taskId) {
+      this.dialog = true;
+      router.push({ name: "TaskDetail", params: taskId });
+    },
+    hideModal() {
+      // this.dialog = false;
+      // router.go(-1);
+      router.push({ name: "Project" });
+    },
   },
   props: ["task"],
 };
@@ -32,5 +53,16 @@ export default {
 
 .done-checkbox {
   border-radius: 100%;
+}
+
+.task-div {
+  width: 100%;
+}
+.task-div:hover {
+  border-radius: 10px;
+  cursor: pointer;
+  background-color: #1c2b82;
+  color: white;
+  padding-left: 10px;
 }
 </style>
