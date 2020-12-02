0fe695753ea750f82d6e09498849468b56cb9536
dimpl@JinYoung <dimple0416@naver.com>
2020-12-03 01:55:48 +0900

    [FE] FEAT: 작업 상세화면 렌더링 초기 작업

--- client/src/components/task/TaskDetail.vue
+++ client/src/components/task/TaskDetail.vue
@@ -1,9 +1,29 @@
 <template>
-  <div>hi!!</div>
+  <div>
+    <div>{{ currentTask.title }} hi!!</div>
+    <div>{{ currentTask }}}</div>
+  </div>
 </template>
 
 <script>
-export default {};
+import { mapGetters, mapActions } from "vuex";
+
+export default {
+  data() {
+    return {
+      dialog: false,
+      taskId: this.$route.params.taskId,
+    };
+  },
+  methods: {
+    ...mapActions(["fetchCurrentTask"]),
+  },
+  computed: mapGetters(["currentTask"]),
+
+  created() {
+    this.fetchCurrentTask(this.$route.params.taskId);
+  },
+};
 </script>
 
 <style></style>
