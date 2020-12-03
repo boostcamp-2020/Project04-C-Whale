458854e2466f529f07a5248aa209c4e067f4ebc4
Jin Young Park <jy2045@sogang.ac.kr>
2020-12-04 05:16:07 +0900

    [FE] FEAT: alert 컴포넌트 및 store 구현
    - v-alert을 API 로직과 연동하여 에러가 나면 alert을 띄워주기 위해 alert
      컴포넌트와 alert의 state 값을 store로 구현

---
+++ client/src/components/common/Alert.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="alert">
+    <v-alert
+      v-if="alert.alert.type"
+      :value="!!alert.alert.message"
+      :type="alert.alert.type"
+      dismissible
+      @input="CLEAR_ALERT()"
+      >{{ alert.alert.message }}</v-alert
+    >
+  </div>
+</template>
+
+<script>
+import { mapState, mapMutations } from "vuex";
+
+export default {
+  computed: {
+    ...mapState(["alert"]),
+  },
+  methods: {
+    ...mapMutations(["CLEAR_ALERT"]),
+  },
+  beforeUpdate() {
+    setTimeout(() => this.CLEAR_ALERT(), 3000);
+  },
+};
+</script>
+
+<style lang="scss">
+.alert {
+  position: fixed;
+  top: 0;
+  left: 50%;
+  transform: translate(-50%, 0);
+}
+</style>
