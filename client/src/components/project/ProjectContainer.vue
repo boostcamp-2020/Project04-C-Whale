e6f5063bb0ee241c5fe66154f98e27f318fbd864
shkilo <kyle.hwan.k@gmail.com>
2020-12-07 11:41:49 +0900

    [FE] FEAT: ProjectContainer의 header 분리/보드 뷰 기능 추가

--- client/src/components/project/ProjectContainer.vue
+++ client/src/components/project/ProjectContainer.vue
@@ -1,45 +1,26 @@
 <template>
   <div class="project-container">
-    <div class="project-header">
-      <v-list-item>
-        <v-list-item-content class="text-h5">
-          <updatable-title
-            v-if="project.title"
-            :originalTitle="project.title"
-            :parent="project"
-            type="project"
-          />
-        </v-list-item-content>
-
-        <v-menu :offset-y="true">
-          <template v-slot:activator="{ on }">
-            <v-list-item-action>
-              <v-btn icon v-on="on">
-                <v-icon>mdi-dots-horizontal</v-icon>
-              </v-btn>
-            </v-list-item-action>
-          </template>
-          <v-list>
-            <v-list-item> 섹션 추가 </v-list-item>
-          </v-list>
-        </v-menu>
-      </v-list-item>
-    </div>
-    <SectionContainer
-      v-for="section in project.sections"
-      :key="section.id"
-      :section="section"
-      :draggingTask="draggingTask"
-      @taskDragStart="taskDragStart"
-      class="mb-5"
+    <ProjectContainerHeader
+      @selectListView="changeToListView"
+      @selectBoardView="changeToBoardView"
+      :project="project"
     />
+
+    <div :class="{ 'board-view': boardView }">
+      <SectionContainer
+        v-for="section in project.sections"
+        :key="section.id"
+        :section="section"
+        class="mb-5"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import { mapActions } from "vuex";
+import ProjectContainerHeader from "./ProjectContainerHeader";
 import SectionContainer from "@/components/project/SectionContainer";
-import UpdatableTitle from "@/components/common/UpdatableTitle";
 
 export default {
   props: {
@@ -47,36 +28,39 @@ export default {
   },
   data() {
     return {
-      projectId: this.$route.params.projectId,
-      draggingTask: {},
+      boardView: false,
     };
   },
   methods: {
     ...mapActions(["updateTaskToDone"]),
-    taskDragStart(task) {
-      this.draggingTask = task;
+    changeToListView() {
+      this.boardView = false;
+    },
+    changeToBoardView() {
+      console.log(11);
+      this.boardView = true;
     },
   },
   components: {
-    UpdatableTitle,
     SectionContainer,
+    ProjectContainerHeader,
   },
 };
 </script>
 
 <style>
-.project-header {
-  min-width: 450px;
-}
-
 .project-container {
   width: 100%;
-  max-width: 600px;
+  /* max-width: 600px; */
 }
 
 .v-dialog {
   max-width: 80%;
   min-height: 80%;
   background-color: white;
+}
+
+.board-view {
+  display: flex;
 }
 </style>
---
+++ client/src/components/project/ProjectContainerHeader.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="project-header">
+    <v-list-item>
+      <v-list-item-content class="text-h5">
+        <UpdatableTitle
+          v-if="project.title"
+          :originalTitle="project.title"
+          :parent="project"
+          type="project"
+        />
+      </v-list-item-content>
+
+      <v-menu :offset-y="true">
+        <template v-slot:activator="{ on }">
+          <v-list-item-action class="mr-6">
+            <v-btn icon v-on="on">
+              <v-icon>mdi-dots-horizontal</v-icon>
+            </v-btn>
+          </v-list-item-action>
+        </template>
+        <v-list>
+          <v-list-item>
+            <v-list-item-title>섹션 추가 </v-list-item-title>
+          </v-list-item>
+          <v-list-item @click="selectListView">
+            <v-list-item-title>보기 형태: 목록 </v-list-item-title>
+          </v-list-item>
+          <v-list-item @click="selectBoardView">
+            <v-list-item-title>보기 형태: 보드 </v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-list-item>
+  </div>
+</template>
+
+<script>
+import UpdatableTitle from "@/components/common/UpdatableTitle";
+export default {
+  props: {
+    project: Object,
+  },
+  data: function () {
+    return {};
+  },
+  methods: {
+    selectListView() {
+      this.$emit("selectListView");
+    },
+    selectBoardView() {
+      this.$emit("selectBoardView");
+    },
+  },
+  components: {
+    UpdatableTitle,
+  },
+};
+</script>
+
+<style>
+.project-header {
+  min-width: 440px;
+}
+</style>
