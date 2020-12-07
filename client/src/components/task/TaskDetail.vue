cd36bd9fd6969ef3ba6125ef1aeff490b6fe5960
dimpl@JinYoung <dimple0416@naver.com>
2020-12-07 11:40:06 +0900

    [FE] FEAT: 작업 상세 페이지 메인 task 화면 표시

--- client/src/components/task/TaskDetail.vue
+++ client/src/components/task/TaskDetail.vue
@@ -10,6 +10,18 @@
           <v-icon>mdi-close</v-icon>
         </v-btn>
       </v-list-item-group>
+
+      <div class="task_container ml-10 mr-10">
+        <task-item :task="task" />
+      </div>
+      <v-tabs v-model="active" color="#1C2B82" slider-color="#07C4A3">
+        <v-tab v-for="tabTitle in this.taskTaps" :key="tabTitle" ripple> {{ tabTitle }} </v-tab>
+        <v-tab-item v-for="childTask in this.task.tasks" :key="childTask">
+          <v-card flat>
+            <v-card-text>{{ text }}</v-card-text>
+          </v-card>
+        </v-tab-item>
+      </v-tabs>
       <div>{{ task }} hi!!</div>
     </v-list-item>
   </v-flex>
@@ -17,15 +29,29 @@
 </template>
 
 <script>
+import TaskItem from "@/components/project/TaskItem";
+
 export default {
+  data() {
+    return {
+      active: null,
+      taskTaps: ["하위 작업", "댓글", "북마크"],
+    };
+  },
   props: {
     task: Object,
     projectTitle: String,
   },
 
+  components: { TaskItem },
+
   methods: {
     hideTaskModal() {
       this.$emit("hideTaskModal");
+    },
+    next() {
+      const active = parseInt(this.active);
+      this.active = active < 2 ? active + 1 : 0;
     },
   },
   computed: {},
