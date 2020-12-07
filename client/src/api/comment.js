2221d2f5d80c38962dfa18819560ce817b8ff004
dimpl@JinYoung <dimple0416@naver.com>
2020-12-08 00:56:48 +0900

    [FE] FEAT: comment update API 추가

--- client/src/api/comment.js
+++ client/src/api/comment.js
@@ -7,6 +7,9 @@ const labelAPI = {
   createComment(data) {
     return myAxios.post(`/task/${data.taskId}/comment`, data);
   },
+  updateComment(comment) {
+    return myAxios.put(`/task/${comment.taskId}/comment/${comment.id}`, comment);
+  },
   deleteComment(comment) {
     return myAxios.delete(`/task/${comment.taskId}/comment/${comment.id}`, comment);
   },
