4b4f3d89492b2656a760fc65bc85e3d2560ff77f
Jin Young Park <jy2045@sogang.ac.kr>
2020-12-07 18:29:34 +0900

    [FE] FEAT: 프로젝트 추가 버튼 UI 정렬

--- client/src/components/menu/ProjectListContainer.vue
+++ client/src/components/menu/ProjectListContainer.vue
@@ -6,7 +6,7 @@
           <v-list-item-content>
             <v-list-item-title class="font-14">프로젝트</v-list-item-title>
           </v-list-item-content>
-          <v-list-item-icon :class="{ 'd-none': !hover }">
+          <v-list-item-icon :class="{ 'd-none': !hover }" @click="openModalEvent">
             <v-icon>mdi-plus</v-icon>
           </v-list-item-icon>
         </v-list-item>
@@ -28,19 +28,54 @@
           >
         </v-list-item-content>
       </v-list-item>
+      <v-list-item
+        @click.stop="dialog = true"
+        inactive
+        class="add-btn"
+        exact-active-class="list-active"
+      >
+        <v-list-item-icon class="mr-4">
+          <v-icon color="red">mdi-plus</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title class="font-14">프로젝트 추가 </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
     </v-list-item-group>
+
+    <add-project :dialog="dialog" v-on:handleModal="dialog = !dialog"></add-project>
   </v-list-group>
 </template>
 
 <script>
+import AddProject from "@/components/project/AddProject.vue";
+
 export default {
   props: {
     projectInfos: Array,
   },
+  components: {
+    AddProject,
+  },
+  data() {
+    return {
+      dialog: false,
+    };
+  },
+  methods: {
+    openModalEvent(e) {
+      e.stopPropagation();
+      this.dialog = true;
+    },
+  },
 };
 </script>
+
 <style lang="scss">
 .list-active {
-  color: black;
+  color: black !important;
+}
+.add-btn {
+  cursor: pointer;
 }
 </style>
---
+++ client/src/components/project/AddProject.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-row justify="center">
+    <v-dialog v-model="dialog" persistent max-width="290">
+      <v-card>
+        <v-card-title class="headline"> Use Google's location service? </v-card-title>
+        <v-card-text
+          >Let Google help apps determine location. This means sending anonymous location data to
+          Google, even when no apps are running.</v-card-text
+        >
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="green darken-1" text @click="sendEvent"> Disagree </v-btn>
+          <v-btn color="green darken-1" text @click="sendEvent"> Agree </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script>
+export default {
+  props: {
+    dialog: Boolean,
+  },
+  methods: {
+    sendEvent() {
+      this.$emit("handleModal");
+    },
+  },
+};
+</script>
