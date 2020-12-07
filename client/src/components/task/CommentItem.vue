f50a66b4224de6be2244904e4afe09e5997fe33c
dimpl@JinYoung <dimple0416@naver.com>
2020-12-08 01:41:01 +0900

    [FE] FEAT: comment update 구현

--- client/src/components/task/CommentItem.vue
+++ client/src/components/task/CommentItem.vue
@@ -1,16 +1,30 @@
 <template>
   <div
     class="comment_item-container d-flex flex-row"
-    @mouseover="showEditComment"
-    @mouseleave="unshowEditComment"
+    @mouseover="showEditUtils"
+    @mouseleave="unshowEditUtils"
   >
-    <v-list-item-group class="flex-grow-1">
+    <v-list-item-group v-if="isVisibleCommentEdit" class="flex-grow-1">
+      <form @submit.prevent="update">
+        <div class="comment-form-data">
+          <v-text-field v-model="newContent" ref="input"></v-text-field>
+          <v-btn type="submit" depressed color="primary" :disabled="newContent.length <= 0"
+            >업데이트</v-btn
+          >
+          <v-btn class="white--text" @click="unshowCommentEdit" depressed color="#07C4A3"
+            >취소</v-btn
+          >
+        </div>
+      </form>
+    </v-list-item-group>
+
+    <v-list-item-group v-else class="flex-grow-1">
       <v-list-item-title> {{ comment.updatedAt }} </v-list-item-title>
       <v-list-item-content> {{ comment.content }} </v-list-item-content>
     </v-list-item-group>
     <!-- <div v-if="showEdit">asd</div> -->
-    <div v-if="showEdit">
-      <v-btn class="ma-2" outlined x-small color="indigo" @click="updateComment">
+    <div v-if="isVisibleEditUtils">
+      <v-btn class="ma-2" outlined x-small color="indigo" @click="showCommentEdit">
         <v-icon>mdi-pencil</v-icon>
         수정
       </v-btn>
@@ -29,19 +43,33 @@ export default {
   },
   data() {
     return {
-      showEdit: false,
+      isVisibleCommentEdit: false,
+      isVisibleEditUtils: false,
+      newContent: this.comment.content,
     };
   },
   methods: {
-    ...mapActions(["deleteComment"]),
-    showEditComment() {
-      this.showEdit = true;
+    ...mapActions(["updateComment", "deleteComment"]),
+    showEditUtils() {
+      if (!this.isVisibleCommentEdit) {
+        this.isVisibleEditUtils = true;
+      }
     },
-    unshowEditComment() {
-      this.showEdit = false;
+    unshowEditUtils() {
+      this.isVisibleEditUtils = false;
     },
-    updateComment() {
-      alert("수정 선택");
+    showCommentEdit() {
+      this.unshowEditUtils();
+      this.isVisibleCommentEdit = true;
+    },
+    unshowCommentEdit() {
+      this.newContent = this.comment.content;
+      this.isVisibleCommentEdit = false;
+    },
+    update() {
+      const updateData = { ...this.comment, content: this.newContent };
+      this.updateComment(updateData);
+      this.unshowCommentEdit();
     },
     remove(commentId) {
       const isRemove = confirm("정말 삭제하시겠습니까?");
