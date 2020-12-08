657185f40cad8ae7a52b593c6b264942c00b0ae1
dimpl@JinYoung <dimple0416@naver.com>
2020-12-09 03:41:20 +0900

    [BE] FEAT: task dto patch validation 추가

--- server/src/models/dto/task.js
+++ server/src/models/dto/task.js
@@ -1,12 +1,10 @@
 const {
   IsInt,
   IsString,
-  IsNotEmpty,
   IsBoolean,
   IsArray,
   MinLength,
   IsDateString,
-  ValidateIf,
   IsUUID,
   IsOptional,
   IsEmpty,
@@ -15,48 +13,62 @@ const errorMessage = require('@models/dto/error-messages');
 const { isAfterToday } = require('@utils/validator');
 
 class TaskDto {
-  @IsEmpty({ groups: ['create'], message: errorMessage.UNNECESSARY_INPUT_ERROR('id') })
+  @IsEmpty({ groups: ['create', 'patch'], message: errorMessage.UNNECESSARY_INPUT_ERROR('id') })
   @IsString({ groups: ['retrieve'], message: errorMessage.TYPE_ERROR('id') })
   @IsUUID('4', { groups: ['retrieve'], message: errorMessage.INVALID_INPUT_ERROR('id') })
   id;
 
-  @IsString({ groups: ['create'], message: errorMessage.TYPE_ERROR('title') })
-  @MinLength(1, { groups: ['create'], message: errorMessage.INVALID_INPUT_ERROR('title') })
+  @IsOptional({ groups: ['patch'] })
+  @IsString({ groups: ['create', 'patch'], message: errorMessage.TYPE_ERROR('title') })
+  @MinLength(1, { groups: ['create', 'patch'], message: errorMessage.INVALID_INPUT_ERROR('title') })
   title;
 
+  @IsOptional({ groups: ['patch'] })
   @IsDateString(
     { strict: true },
-    { groups: ['create'], message: errorMessage.TYPE_ERROR('dueDate') },
+    { groups: ['create', 'patch'], message: errorMessage.TYPE_ERROR('dueDate') },
   )
-  @isAfterToday('dueDate', { groups: ['create'], message: errorMessage.DUEDATE_ERROR })
+  @isAfterToday('dueDate', { groups: ['create', 'patch'], message: errorMessage.DUEDATE_ERROR })
   dueDate;
 
-  @IsInt()
-  @IsNotEmpty()
+  @IsOptional({ groups: ['patch'] })
+  @IsInt({ groups: ['patch'], message: errorMessage.TYPE_ERROR('position') })
   position;
 
-  @IsBoolean()
-  @IsNotEmpty()
+  @IsOptional({ groups: ['patch'] })
+  @IsBoolean({ groups: ['patch'], message: errorMessage.TYPE_ERROR('position') })
   isDone;
 
-  @IsOptional({ groups: ['create'] })
-  @IsString({ groups: ['create'], message: errorMessage.TYPE_ERROR('sectionId') })
-  @IsUUID('4', { groups: ['create'], message: errorMessage.INVALID_INPUT_ERROR('parentId') })
+  @IsOptional({ groups: ['create', 'patch'] })
+  @IsString({ groups: ['create', 'patch'], message: errorMessage.TYPE_ERROR('sectionId') })
+  @IsUUID('4', {
+    groups: ['create', 'patch'],
+    message: errorMessage.INVALID_INPUT_ERROR('parentId'),
+  })
   parentId;
 
-  @IsString({ groups: ['create'], message: errorMessage.TYPE_ERROR('sectionId') })
-  @IsUUID('4', { groups: ['create'], message: errorMessage.INVALID_INPUT_ERROR('sectionId') })
+  @IsOptional({ groups: ['patch'] })
+  @IsString({ groups: ['create', 'patch'], message: errorMessage.TYPE_ERROR('sectionId') })
+  @IsUUID('4', {
+    groups: ['create', 'patch'],
+    message: errorMessage.INVALID_INPUT_ERROR('sectionId'),
+  })
   sectionId;
 
-  @IsString({ groups: ['create'], message: errorMessage.TYPE_ERROR('projectId') })
-  @IsUUID('4', { groups: ['create'], message: errorMessage.INVALID_INPUT_ERROR('projectId') })
+  @IsOptional({ groups: ['patch'] })
+  @IsString({ groups: ['create', 'patch'], message: errorMessage.TYPE_ERROR('projectId') })
+  @IsUUID('4', {
+    groups: ['create', 'patch'],
+    message: errorMessage.INVALID_INPUT_ERROR('projectId'),
+  })
   projectId;
 
-  @IsString()
-  @IsUUID('4')
+  @IsOptional({ groups: ['patch'] })
+  @IsUUID('4', { groups: ['patch'], message: errorMessage.INVALID_INPUT_ERROR('priorityId') })
   priorityId;
 
-  @IsUUID('4')
+  @IsOptional({ groups: ['patch'] })
+  @IsUUID('4', { groups: ['patch'], message: errorMessage.INVALID_INPUT_ERROR('alarmId') })
   alarmId;
 
   @IsArray
