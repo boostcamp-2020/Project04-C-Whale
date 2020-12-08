444100d010659193dfdd2c26f7cafefa97b2defc
woongs <ccw1021@naver.com>
2020-12-09 03:04:51 +0900

    [iOS] feat: Project 생성자 추가
    - 현재 decode init밖에 쓸 수가 없음
    - coder로 잘못 생성되었을 때 빈 Project라도 만들어 주기 위해 기본 Init이 필요

--- iOS/HalgoraeDO/Sources/Models/Project.swift
+++ iOS/HalgoraeDO/Sources/Models/Project.swift
@@ -14,6 +14,20 @@ class Project: Codable {
     var isFavorite: Bool?
     var isList: Bool?
     var sections: [Section]?
+    
+    init(id: String? = UUID().uuidString,
+         title: String,
+         taskCount: Int = 0,
+         isFavorite: Bool? = false,
+         isList: Bool? = false,
+         sections: [Section]? = []) {
+        self.id = id
+        self.title = title
+        self.taskCount = taskCount
+        self.isFavorite = isFavorite
+        self.isList = isList
+        self.sections = sections
+    }
 }
 
 extension Project: Hashable {
