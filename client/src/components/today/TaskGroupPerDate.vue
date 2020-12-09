661de862449db06e909ae7e58adf447cecc7eb76
Jin Young Park <jy2045@sogang.ac.kr>
2020-12-10 04:18:19 +0900

    [FE] REFACTOR: TodayTasksContainer 리팩토링 - TaskGroupPerDate 하위 컴포넌트 생성

---
+++ client/src/components/today/TaskGroupPerDate.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-list class="mb-5" v-show="tasks.length > 0">
+    <v-list-item class="font-weight-black text-h6">{{
+      type === "expired" ? "기한이지난" : "오늘"
+    }}</v-list-item>
+    <div v-for="task in tasks" :key="task.id" class="task-container">
+      <TaskItem :task="task" />
+      <v-divider />
+      <div class="childTaskContainer ml-10" v-for="childTask in task.tasks" :key="childTask.id">
+        <TaskItem :task="childTask" />
+      </div>
+    </div>
+    <AddTask />
+  </v-list>
+</template>
+
+<script>
+import TaskItem from "@/components/project/TaskItem";
+import AddTask from "@/components/project/AddTask";
+
+export default {
+  props: {
+    tasks: Array,
+    type: String,
+  },
+  components: { TaskItem, AddTask },
+};
+</script>
--- client/src/components/today/TodayTasksContainer.vue
+++ client/src/components/today/TodayTasksContainer.vue
@@ -1,43 +1,12 @@
 <template>
   <div class="project-container">
-    <v-list class="mb-5" v-if="expiredTasks.length > 0">
-      <v-list-item class="font-weight-black text-h6"> 기한이 지난 </v-list-item>
-
-      <div v-for="task in expiredTasks" :key="task.id" class="task-container">
-        <task-item :task="task" />
-
-        <v-divider />
-
-        <div class="childTaskContainer ml-10" v-for="childTask in task.tasks" :key="childTask.id">
-          <task-item :task="childTask" />
-        </div>
-      </div>
-      <!-- <AddTask :projectId="managedProject.id" /> -->
-      <!-- <add-task :projectId="section.projectId" :sectionId="section.id" /> -->
-    </v-list>
-    <v-list class="mb-5">
-      <v-list-item class="font-weight-black text-h6"> 오늘 </v-list-item>
-
-      <div v-for="task in todayTasks" :key="task.id" class="task-container">
-        <task-item :task="task" />
-
-        <v-divider />
-
-        <div class="childTaskContainer ml-10" v-for="childTask in task.tasks" :key="childTask.id">
-          <task-item :task="childTask" />
-        </div>
-      </div>
-      <!-- <AddTask :projectId="managedProject.id" /> -->
-
-      <!-- <add-task :projectId="section.projectId" :sectionId="section.id" /> -->
-    </v-list>
+    <TaskGroupPerDate :tasks="expiredTasks" type="expired" />
+    <TaskGroupPerDate :tasks="todayTasks" type="today" />
   </div>
 </template>
 
 <script>
-import { mapActions, mapGetters } from "vuex";
-import TaskItem from "@/components/project/TaskItem";
-import AddTask from "@/components/project/AddTask";
+import TaskGroupPerDate from "@/components/today/TaskGroupPerDate";
 
 export default {
   props: {
@@ -47,12 +16,6 @@ export default {
   data() {
     return {};
   },
-  computed: {
-    ...mapGetters(["managedProject"]),
-  },
-  methods: {
-    ...mapActions(["updateTaskToDone"]),
-  },
-  components: { TaskItem, AddTask },
+  components: { TaskGroupPerDate },
 };
 </script>
