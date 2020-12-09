439bca709872d0664fff8c0ea4938aa37559dc0b
Jin Young Park <jy2045@sogang.ac.kr>
2020-12-09 23:16:58 +0900

    [FE] FEAT: whale bookmark API 테스트

--- whale/background.js
+++ whale/background.js
@@ -4,25 +4,30 @@ whale.runtime.onConnectExternal.addListener((port) => {
   };
 
   whale.contextMenus.create({
-    id: '123',
+    id: "123",
     title: `할고래DO 할일로 추가`,
-    contexts: ['selection'],
+    contexts: ["selection"],
   });
 
   whale.contextMenus.onClicked.addListener((info) => {
-    if (info.menuItemId == '123') {
+    if (info.menuItemId == "123") {
       port.postMessage(info);
     }
   });
 });
 
-whale.runtime.onMessageExternal.addListener((message, sender, sendResponse) => {
-  switch (message) {
-    case 'getCurrentTabUrl':
+whale.runtime.onMessageExternal.addListener(({ type, data }, sender, sendResponse) => {
+  switch (type) {
+    case "getCurrentTabUrl":
       whale.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
         const currentTab = tabs[0];
         sendResponse({ url: currentTab.url, title: currentTab.title });
       });
+      break;
+
+    case "createBookmark":
+      whale.bookmarks.create({ title: data.title, url: data.url });
+      sendResponse("북마크가 추가되었습니다.");
       break;
   }
 });
