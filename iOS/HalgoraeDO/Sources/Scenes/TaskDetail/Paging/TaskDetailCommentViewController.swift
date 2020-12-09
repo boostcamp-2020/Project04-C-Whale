e4b97af8c50b3e775a6a6f375f7d192d6a1dc008
woongs <ccw1021@naver.com>
2020-12-10 06:12:42 +0900

    [iOS] feat: TaskDetailComment뷰컨 로직 설정 및 데이터소스 기본세팅

--- iOS/HalgoraeDO/Sources/Scenes/TaskDetail/Paging/TaskDetailCommentViewController.swift
+++ iOS/HalgoraeDO/Sources/Scenes/TaskDetail/Paging/TaskDetailCommentViewController.swift
@@ -12,14 +12,83 @@ protocol TaskDetailCommentDisplayLogic: class {
 }
 
 class TaskDetailCommentViewController: UIViewController {
+    
+    // MARK: - Properties
 
+    private var task: Task?
     private var interactor: TaskDetailBusinessLogic?
-
+    private var dataSource: UICollectionViewDiffableDataSource<String, TaskDetailModels.CommentVM>!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureCollectionView()
+        configureDataSource()
     }
-    func configure(interactor: TaskDetailBusinessLogic) {
+    
+    // MARK: - Initialize
+    
+    func configure(interactor: TaskDetailBusinessLogic, task: Task) {
         self.interactor = interactor
+        self.task = task
+    }
+    
+// MARK: - Configure CollectionView Layout
+
+private extension TaskDetailCommentViewController {
+    
+    func configureCollectionView() {
+        commentCollectionView.collectionViewLayout = generateLayout()
+        
+    }
+    
+    func generateLayout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                               heightDimension: .estimated(44))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                              heightDimension: .estimated(44))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize,
+                                                         subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = .init(top: 8, leading: 16, bottom: 8, trailing: 16)
+        section.interGroupSpacing = 20
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+    }
+}
+
+// MARK: - Configure CollectionView Data Source
+
+private extension TaskDetailCommentViewController {
+    
+    func configureDataSource() {
+        
+        let cellRegistration = UICollectionView.CellRegistration<TaskCommentCell, TaskDetailModels.CommentVM> { (cell, _: IndexPath, taskItem) in
+            cell.viewModel = taskItem
+        }
+        
+        let imageCellRegistration = UICollectionView.CellRegistration<TaskCommentImageCell, TaskDetailModels.CommentVM> { (cell, _: IndexPath, taskItem) in
+            cell.viewModel = taskItem
+        }
+        
+        dataSource = UICollectionViewDiffableDataSource<String, TaskDetailModels.CommentVM>(collectionView: commentCollectionView, cellProvider: { (collectionView, indexPath, task) -> UICollectionViewCell? in
+            
+            if task.isImage {
+                return collectionView.dequeueConfiguredReusableCell(using: imageCellRegistration, for: indexPath, item: task)
+            } else {
+                return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: task)
+            }
+            
+        })
+    }
+    
+    func snapshot(taskItems: [TaskDetailModels.CommentVM]) -> NSDiffableDataSourceSectionSnapshot<TaskDetailModels.CommentVM> {
+        var snapshot = NSDiffableDataSourceSectionSnapshot<TaskDetailModels.CommentVM>()
+        snapshot.append(taskItems)
+        
+        return snapshot
     }
 }
 
