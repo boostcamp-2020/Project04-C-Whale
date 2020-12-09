4103ea577b825e22adf1c829fae9da9515626299
dimpl@JinYoung <dimple0416@naver.com>
2020-12-10 01:53:19 +0900

    [BE] FEAT: comment update validation 추가

--- server/src/models/dto/comment.js
+++ server/src/models/dto/comment.js
@@ -1,31 +1,30 @@
 const {
-  IsInt,
   IsString,
   IsBoolean,
   MinLength,
-  IsDateString,
-  IsUUID,
   IsOptional,
   IsEmpty,
+  IsNotEmpty,
 } = require('class-validator');
 const errorMessage = require('@utils/error-messages');
 
 class CommentDto {
   @IsEmpty({ groups: ['create', 'update'], message: errorMessage.UNNECESSARY_INPUT_ERROR('id') })
-  @IsString()
-  @IsUUID()
   id;
 
-  @IsString({ groups: ['create'], message: errorMessage.TYPE_ERROR('content') })
-  @MinLength(1, { groups: ['create'], message: errorMessage.INVALID_INPUT_ERROR('content') })
+  @IsNotEmpty({ groups: ['update'], message: errorMessage.NECESSARY_INPUT_ERROR('content') })
+  @IsString({ groups: ['create', 'update'], message: errorMessage.TYPE_ERROR('content') })
+  @MinLength(1, {
+    groups: ['create', 'update'],
+    message: errorMessage.INVALID_INPUT_ERROR('content'),
+  })
   content;
 
+  @IsNotEmpty({ groups: ['update'], message: errorMessage.NECESSARY_INPUT_ERROR('isImage') })
   @IsOptional({ groups: ['create'] })
-  @IsBoolean({ groups: ['create'], message: errorMessage.TYPE_ERROR('isImage') })
+  @IsBoolean({ groups: ['create', 'update'], message: errorMessage.TYPE_ERROR('isImage') })
   isImage;
 
-  @IsString()
-  @IsUUID()
   taskId;
 }
 
