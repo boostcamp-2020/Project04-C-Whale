02db4147f0a6d32087e18d5418fa8e57924d1f80
dimpl@JinYoung <dimple0416@naver.com>
2020-12-10 02:46:26 +0900

    [BE] REFACTOR: IsNotEmpty 제거

--- server/src/models/dto/comment.js
+++ server/src/models/dto/comment.js
@@ -1,18 +1,10 @@
-const {
-  IsString,
-  IsBoolean,
-  MinLength,
-  IsOptional,
-  IsEmpty,
-  IsNotEmpty,
-} = require('class-validator');
+const { IsString, IsBoolean, IsOptional, IsEmpty, MinLength } = require('class-validator');
 const errorMessage = require('@utils/error-messages');
 
 class CommentDto {
   @IsEmpty({ groups: ['create', 'update'], message: errorMessage.UNNECESSARY_INPUT_ERROR('id') })
   id;
 
-  @IsNotEmpty({ groups: ['update'], message: errorMessage.NECESSARY_INPUT_ERROR('content') })
   @IsString({ groups: ['create', 'update'], message: errorMessage.TYPE_ERROR('content') })
   @MinLength(1, {
     groups: ['create', 'update'],
@@ -20,7 +12,6 @@ class CommentDto {
   })
   content;
 
-  @IsNotEmpty({ groups: ['update'], message: errorMessage.NECESSARY_INPUT_ERROR('isImage') })
   @IsOptional({ groups: ['create'] })
   @IsBoolean({ groups: ['create', 'update'], message: errorMessage.TYPE_ERROR('isImage') })
   isImage;
