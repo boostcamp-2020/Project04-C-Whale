f61e960d89e720acd38a1370eb44176e928b3f41
woongs <ccw1021@naver.com>
2020-12-09 17:52:20 +0900

    [iOS] feat: TaskListWorker에 networkManager 추가

--- iOS/HalgoraeDO/Sources/Scenes/TaskList/TaskListWorker.swift
+++ iOS/HalgoraeDO/Sources/Scenes/TaskList/TaskListWorker.swift
@@ -8,24 +8,20 @@
 import Foundation
 
 class TaskListWorker {
-    var tasks: [Task] = [
-        Task(title: "할고래두", subTasks: [
-                Task(title: "hihi"),
-                Task(title: "hihi2"),
-                Task(title: "hehehe3"),
-            ]),
-        Task(title: "당연하지당연하지당연하지당연하지당연하지당연하지당연하지당연하지당연하지당연하지당연하지당연하지당연하지당연하지당연하지당연하지당연하지당연하지당연하지당연하지당연하지당연하지당연하지당연하지당연하지당연하지당연하지당연하지당연하지"),
-        Task(title: "두 말하면 섭함"),
-        Task(title: "할고래두 진짜!", subTasks: [
-                Task(title: "hihiㅎㅎ"),
-                Task(title: "hihi2ㅎㅎㅎ"),
-                Task(title: "hehehe3ㅎㅎㅎㅎ"),
-            ])
-    ]
     
-    func getTasks() -> [Task] {
-        return tasks
+    let networkManager: NetworkDispatcher
+    
+    init(sessionManager: SessionManagerProtocol) {
+        networkManager = NetworkManager(sessionManager: sessionManager)
     }
+    
+    func request<T: Decodable>(endPoint: ProjectEndPoint, completion: @escaping ((T?, NetworkError?) -> Void)) {
+        networkManager.fetchData(endPoint) { (result: T?, error: NetworkError?) in
+            completion(result, error)
+        }
+    }
+    
+    // func fetchTasks(End)
     
     func changeFinish(task: Task, postion: Int, parentPosition: Int?) {
         
