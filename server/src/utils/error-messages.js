6208b0ee9eb6032faeebe3a2302f49fcd6d52a2d
dimpl@JinYoung <dimple0416@naver.com>
2020-12-10 03:06:16 +0900

    [BE] FEAT: forbidden error 메시지 추가

--- server/src/utils/error-messages.js
+++ server/src/utils/error-messages.js
@@ -5,4 +5,5 @@ module.exports = {
   INVALID_INPUT_ERROR: property => `${property}값이 올바르지 않습니다.`,
   NOT_FOUND_ERROR: property => `존재하지 않는 ${property}입니다.`,
   DUEDATE_ERROR: 'dueDate는 현재시간보다 이전을 설정할 수 없습니다.',
+  FORBIDDEN: 'Forbidden',
 };
