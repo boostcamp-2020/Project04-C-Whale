d6b3b466f0ba8f717bed8128b2e7c90f3bd985a9
dimpl@JinYoung <dimple0416@naver.com>
2020-12-09 23:25:32 +0900

    [BE] FEAT: comment get 반환 수정, create validation 추가

--- server/src/controllers/comment.js
+++ server/src/controllers/comment.js
@@ -1,14 +1,25 @@
+const CommentDto = require('@models/dto/comment');
 const commentService = require('@services/comment');
+const { validator, getErrorMsg } = require('@utils/validator');
 const { asyncTryCatch } = require('@utils/async-try-catch');
 const { responseHandler } = require('@utils/handler');
 
 const getComments = asyncTryCatch(async (req, res) => {
   const comments = await commentService.retrieveAllByTaskId(req.params.taskId);
 
-  responseHandler(res, 200, comments);
+  responseHandler(res, 200, { comments });
 });
 
 const createComment = asyncTryCatch(async (req, res) => {
+  try {
+    await validator(CommentDto, req.body, { groups: ['create'] });
+  } catch (errs) {
+    const message = getErrorMsg(errs);
+    const err = new Error(message);
+    err.status = 400;
+    throw err;
+  }
+
   await commentService.create(req.params.taskId, req.body);
 
   responseHandler(res, 201, { message: 'ok' });
