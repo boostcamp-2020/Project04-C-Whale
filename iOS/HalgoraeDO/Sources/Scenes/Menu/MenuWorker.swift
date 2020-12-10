52e6e83d0c931f18fad40b46a6aee8c8216efa81
woongs <ccw1021@naver.com>
2020-12-10 12:33:45 +0900

    [iOS] fix: MenuWorker create 케이스 처리 및 에러를 던지지 않고 직접 처리
    - Worker에서 에러를 핸들링하고 Interactor에서는 기대하는 결과값만 받도록 수정했습니다
    - create 같은 경우 요청을 하고 조회를 한 번더 해주는 방식으로 합의를 해서 수정했습니다

--- iOS/HalgoraeDO/Sources/Scenes/Menu/MenuWorker.swift
+++ iOS/HalgoraeDO/Sources/Scenes/Menu/MenuWorker.swift
@@ -15,9 +15,40 @@ class MenuWorker {
         networkManager = NetworkManager(sessionManager: sessionManager)
     }
     
-    func request<T: Decodable>(endPoint: ProjectEndPoint, completion: @escaping ((T?, NetworkError?) -> Void)) {
+    func request<T: Decodable>(endPoint: ProjectEndPoint, completion: @escaping (T?) -> Void) {
+        
+        if case ProjectEndPoint.create(_) = endPoint {
+            requestCreate(endPoint: endPoint) { (result: T?) in
+                completion(result)
+            }
+            return
+        }
+        
         networkManager.fetchData(endPoint) { (result: T?, error: NetworkError?) in
-            completion(result, error)
+            guard error == nil else {
+                #if DEBUG
+                print(error ?? "error is null")
+                #endif
+                completion(nil)
+                return
+            }
+            completion(result)
+        }
+    }
+    
+    private func requestCreate<T: Decodable>(endPoint: ProjectEndPoint, completion: @escaping ((T?) -> Void)) {
+        networkManager.fetchData(endPoint) { [weak self] (response: ResponseMessage?, error) in
+            guard error == nil else {
+                #if DEBUG
+                print("response msg: \(String(describing: response))")
+                print(error ?? "error is null")
+                #endif
+                completion(nil)
+                return
+            }
+            self?.networkManager.fetchData(ProjectEndPoint.getAll) { (result: T?, error) in
+                completion(result)
+            }
         }
     }
 }
