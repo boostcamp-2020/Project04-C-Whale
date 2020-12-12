f1e55cdd9083ad8eab22fdfc46fa2e63077381c5
woongs <ccw1021@naver.com>
2020-12-12 11:28:07 +0900

    [iOS] feat: TaskListWorker 수정

--- iOS/HalgoraeDO/Sources/Scenes/TaskList/TaskListWorker.swift
+++ iOS/HalgoraeDO/Sources/Scenes/TaskList/TaskListWorker.swift
@@ -15,9 +15,32 @@ class TaskListWorker {
         networkManager = NetworkManager(sessionManager: sessionManager)
     }
     
-    func request<T: Decodable>(endPoint: ProjectEndPoint, completion: @escaping ((T?, NetworkError?) -> Void)) {
-        networkManager.fetchData(endPoint) { (result: T?, error: NetworkError?) in
-            completion(result, error)
+    func request<T: Decodable>(endPoint: ProjectEndPoint, completion: @escaping ((T?) -> Void)) {
+        networkManager.fetchData(endPoint) { (result: ResponseProject<T>?, error: NetworkError?) in
+            guard error == nil else {
+                #if DEBUG
+                print(error ?? "error is null")
+                #endif
+                completion(nil)
+                return
+            }
+            completion(result?.project)
+        }
+    }
+    
+    func requestPostAndGet<T: Decodable>(post postEndPoint: EndPointType, get getEndPoint: EndPointType, completion: @escaping (T?) -> Void) {
+        networkManager.fetchData(postEndPoint) { [weak self] (response: ResponseMessage?, error) in
+            guard error == nil else {
+                #if DEBUG
+                print("response msg: \(String(describing: response))")
+                print(error ?? "error is null")
+                #endif
+                completion(nil)
+                return
+            }
+            self?.networkManager.fetchData(getEndPoint) { (result: T?, error) in
+                completion(result)
+            }
         }
     }
     
