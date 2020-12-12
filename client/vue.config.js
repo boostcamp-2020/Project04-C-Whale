ff37ed2d82f9e0aafaf6c43a1d77e6b32e5a9e8b
Jin Young Park <jy2045@sogang.ac.kr>
2020-12-12 18:24:58 +0900

    [FE] REFACTOR: html webpack plugin title 변경

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
   lintOnSave: false,
+  chainWebpack: (config) => {
+    config.plugin("html").tap((args) => {
+      args[0].title = "HalgoraeDo";
+      return args;
+    });
+  },
 };
