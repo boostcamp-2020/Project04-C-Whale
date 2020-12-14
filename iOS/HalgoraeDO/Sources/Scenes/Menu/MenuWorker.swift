401035df2f41a648d6fd0aedee93e79088979043
woongs <ccw1021@naver.com>
2020-12-14 17:22:48 +0900

    [iOS] feat: MenuWork에 오프라인 기능 추가

--- iOS/HalgoraeDO/Sources/Scenes/Menu/MenuWorker.swift
+++ iOS/HalgoraeDO/Sources/Scenes/Menu/MenuWorker.swift
@@ -10,12 +10,22 @@ import Foundation
 class MenuWorker {
     
     let networkManager: NetworkDispatcher
+    let storage: ProjectStorageType
+    private var networkAvailable: Bool {
+        return NetworkMonitor.shared.isAvailable
+    }
     
-    init(sessionManager: SessionManagerProtocol) {
-        networkManager = NetworkManager(sessionManager: sessionManager)
+    init(sessionManager: SessionManagerProtocol, storage: ProjectStorageType) {
+        self.networkManager = NetworkManager(sessionManager: sessionManager)
+        self.storage = storage
     }
     
     func request<T: Decodable>(endPoint: ProjectEndPoint, completion: @escaping (T?) -> Void) {
+        guard networkAvailable else {
+            requestStorage(endPoint: endPoint, completion: completion)
+            return
+        }
+        
         networkManager.fetchData(endPoint) { (result: ResponseProject<T>?, error: NetworkError?) in
             guard error == nil else {
                 #if DEBUG
@@ -28,7 +38,12 @@ class MenuWorker {
         }
     }
     
-    func requestPostAndGet<T: Decodable>(post postEndPoint: EndPointType, get getEndPoint: EndPointType, completion: @escaping (T?) -> Void) {
+    func requestPostAndGet<T: Decodable>(post postEndPoint: ProjectEndPoint, get getEndPoint: EndPointType, completion: @escaping (T?) -> Void) {
+        guard networkAvailable else {
+            requestStorage(endPoint: postEndPoint, completion: completion)
+            return
+        }
+        
         networkManager.fetchData(postEndPoint) { [weak self] (response: ResponseMessage?, error) in
             guard error == nil else {
                 #if DEBUG
@@ -49,6 +64,62 @@ class MenuWorker {
                 }
                 completion(result?.projectInfos)
             }
+        }
+    }
+    
+    func updateProjects(_ projects: [Project]) {
+        guard networkAvailable else { return }
+        storage.updateProjects(to: projects)
+    }
+    
+    func requestStorage<T: Decodable>(endPoint: ProjectEndPoint, completion: @escaping (T?) -> Void) {
+        switch endPoint {
+            case .create(let data):
+                guard let projectFields = try? JSONDecoder().decode(MenuModels.ProjectFields.self, from: data) else { return }
+                storage.createProject(for: projectFields)
+                storage.saveEndPoint(endPoint)
+                storage.fetchProjectList { (projects, error) in
+                    guard error == nil else {
+                        #if DEBUG
+                        print("fetch fail from storage, \(error!)")
+                        #endif
+                        completion(nil)
+                        return
+                    }
+                    completion(projects as? T ?? nil)
+                }
+            case .delete(let id):
+                storage.deleteProject(id: id)
+                storage.saveEndPoint(endPoint)
+            case .get(let projectId):
+                let project = storage.fetchProject(id: projectId)
+                completion(project as? T ?? nil)
+            case .getAll:
+                storage.fetchProjectList { (projects, error) in
+                    guard error == nil else {
+                        #if DEBUG
+                        print("fetch fail from storage, \(error!)")
+                        #endif
+                        completion(nil)
+                        return
+                    }
+                    completion(projects as? T ?? nil)
+                }
+            case .update(let id, let data):
+                guard let projectFields = try? JSONDecoder().decode(MenuModels.ProjectFields.self, from: data) else { return }
+                storage.updateProject(id: id, for: projectFields)
+                storage.saveEndPoint(endPoint)
+                storage.fetchProjectList { (projects, error) in
+                    guard error == nil else {
+                        #if DEBUG
+                        print("fetch fail from storage, \(error!)")
+                        #endif
+                        completion(nil)
+                        return
+                    }
+                    completion(projects as? T ?? nil)
+                }
+            default: break
         }
     }
 }
