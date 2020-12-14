40610f12d81be20657d1f9adb3334b72703a1241
dimpl@JinYoung <dimple0416@naver.com>
2020-12-14 13:42:39 +0900

    [BE] FEAT: params validation 추가 & custom 에러 적용

--- server/src/controllers/task.js
+++ server/src/controllers/task.js
@@ -1,18 +1,18 @@
 const TaskDto = require('@models/dto/task');
 const taskService = require('@services/task');
-const { validator, getErrorMsg } = require('@utils/validator');
+const { validator, getTypeError } = require('@utils/validator');
 const { asyncTryCatch } = require('@utils/async-try-catch');
 const { responseHandler } = require('@utils/handler');
+const ParamsValidator = require('@utils/params-validator');
 
 const getTaskById = asyncTryCatch(async (req, res) => {
   const id = req.params.taskId;
   try {
+    await validator(ParamsValidator, req.params, {});
     await validator(TaskDto, { id }, { groups: ['retrieve'] });
   } catch (errs) {
-    const message = getErrorMsg(errs);
-    const err = new Error(message);
-    err.status = 400;
-    throw err;
+    const validationError = getTypeError(errs);
+    throw validationError;
   }
 
   const task = await taskService.retrieveById({ id, userId: req.user.id });
@@ -28,12 +28,11 @@ const getAllTasks = asyncTryCatch(async (req, res) => {
 
 const createTask = asyncTryCatch(async (req, res) => {
   try {
+    await validator(ParamsValidator, req.params);
     await validator(TaskDto, req.body, { groups: ['create'] });
   } catch (errs) {
-    const message = getErrorMsg(errs);
-    const err = new Error(message);
-    err.status = 400;
-    throw err;
+    const validationError = getTypeError(errs);
+    throw validationError;
   }
   const { projectId, sectionId } = req.params;
   const task = { ...req.body, projectId, sectionId, userId: req.user.id };
@@ -47,12 +46,11 @@ const updateTask = asyncTryCatch(async (req, res) => {
   const task = { ...req.body };
 
   try {
+    await validator(ParamsValidator, req.params);
     await validator(TaskDto, task, { groups: ['patch'] });
   } catch (errs) {
-    const message = getErrorMsg(errs);
-    const err = new Error(message);
-    err.status = 400;
-    throw err;
+    const validationError = getTypeError(errs);
+    throw validationError;
   }
 
   await taskService.update({ id: taskId, userId: req.user.id, ...task });
@@ -60,6 +58,7 @@ const updateTask = asyncTryCatch(async (req, res) => {
 });
 
 const deleteTask = asyncTryCatch(async (req, res) => {
+  await validator(ParamsValidator, req.params);
   await taskService.remove(req.params.taskId);
   responseHandler(res, 200, { message: 'ok' });
 });
