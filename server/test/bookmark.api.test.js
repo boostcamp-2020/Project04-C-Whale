c27d0180b3938e9bc58542595032917f5e07dd06
dimpl@JinYoung <dimple0416@naver.com>
2020-12-14 22:11:53 +0900

    [BE] TEST: bookmark post 테스트 케이스 작성

--- server/test/bookmark.api.test.js
+++ server/test/bookmark.api.test.js
@@ -1,6 +1,7 @@
 require('module-alias/register');
 const request = require('supertest');
 const app = require('@root/app');
+const status = require('@test/response-status');
 const seeder = require('@test/test-seed');
 const { customError } = require('@utils/custom-error');
 const { createJWT } = require('@utils/auth');
@@ -111,22 +112,200 @@ describe('get bookmark', () => {
   });
 });
 
-// describe('post bookmark', () => {
-//   it('bookmark 생성 성공', async done => {
-//     // given
-//     const taskId = seeder.tasks[0].id;
-//     const data = { url: 'https://www.naver.com', title: '네이버' };
+describe('post bookmark', () => {
+  it('bookmark 생성 성공', async done => {
+    // given
+    const taskId = seeder.tasks[0].id;
+    const data = { url: 'https://www.naver.com', title: '네이버' };
 
-//     // when
-//     const res = request(app)
-//       .post(`/api/task/${taskId}/bookmark`)
-//       .set('Authorization', `Bearer ${createJWT(seeder.users[0])}`)
-//       .send(data);
+    // when
+    const res = await request(app)
+      .post(`/api/task/${taskId}/bookmark`)
+      .set('Authorization', `Bearer ${createJWT(seeder.users[0])}`)
+      .send(data);
 
-//     // then
+    // then
+    expect(res.status).toBe(status.SUCCESS.POST.CODE);
+    expect(res.body.message).toBe(status.SUCCESS.MSG);
+    done();
+  });
+  it('title 없이 생성', async done => {
+    // given
+    const taskId = seeder.tasks[0].id;
+    const data = { url: 'https://www.naver.com' };
 
-//   });
-// });
+    // when
+    const res = await request(app)
+      .post(`/api/task/${taskId}/bookmark`)
+      .set('Authorization', `Bearer ${createJWT(seeder.users[0])}`)
+      .send(data);
+
+    // then
+    expect(res.status).toBe(status.SUCCESS.POST.CODE);
+    expect(res.body.message).toBe(status.SUCCESS.MSG);
+    done();
+  });
+  it('request에 id가 포함된 경우', async done => {
+    // given
+    const taskId = seeder.tasks[0].id;
+    const data = { id: 'hi', url: 'https://www.naver.com', title: '네이버' };
+    const expectedError = customError.UNNECESSARY_INPUT_ERROR('id');
+    // when
+    const res = await request(app)
+      .post(`/api/task/${taskId}/bookmark`)
+      .set('Authorization', `Bearer ${createJWT(seeder.users[0])}`)
+      .send(data);
+
+    // then
+    expect(res.status).toBe(expectedError.status);
+    expect(res.body.message).toBe(expectedError.message);
+    expect(res.body.code).toBe(expectedError.code);
+    done();
+  });
+  it('request에 taskId가 포함된 경우', async done => {
+    // given
+    const taskId = seeder.tasks[0].id;
+    const data = { taskId, url: 'https://www.naver.com', title: '네이버' };
+    const expectedError = customError.UNNECESSARY_INPUT_ERROR('taskId');
+    // when
+    const res = await request(app)
+      .post(`/api/task/${taskId}/bookmark`)
+      .set('Authorization', `Bearer ${createJWT(seeder.users[0])}`)
+      .send(data);
+
+    // then
+    expect(res.status).toBe(expectedError.status);
+    expect(res.body.message).toBe(expectedError.message);
+    expect(res.body.code).toBe(expectedError.code);
+    done();
+  });
+  it('url 타입이 잘못된 경우', async done => {
+    // given
+    const taskId = seeder.tasks[0].id;
+    const data = { url: 1 };
+    const expectedError = customError.TYPE_ERROR('url');
+
+    // when
+    const res = await request(app)
+      .post(`/api/task/${taskId}/bookmark`)
+      .set('Authorization', `Bearer ${createJWT(seeder.users[0])}`)
+      .send(data);
+
+    // then
+    expect(res.status).toBe(expectedError.status);
+    expect(res.body.message).toBe(expectedError.message);
+    expect(res.body.code).toBe(expectedError.code);
+    done();
+  });
+  it('url 형식이 잘못된 경우', async done => {
+    // given
+    const taskId = seeder.tasks[0].id;
+    const data = { url: 'https://www.naver.com' };
+    const expectedError = customError.INVALID_INPUT_ERROR('url');
+
+    // when
+    const res = await request(app)
+      .post(`/api/task/${taskId}/bookmark`)
+      .set('Authorization', `Bearer ${createJWT(seeder.users[0])}`)
+      .send(data);
+
+    // then
+    expect(res.status).toBe(expectedError.status);
+    expect(res.body.message).toBe(expectedError.message);
+    expect(res.body.code).toBe(expectedError.code);
+    done();
+  });
+  it('url이 없는 경우', async done => {
+    // given
+    const taskId = seeder.tasks[0].id;
+    const data = { title: '네이버 웨일로 가봅시다!' };
+    const expectedError = customError.NECESSARY_INPUT_ERROR('url');
+
+    // when
+    const res = await request(app)
+      .post(`/api/task/${taskId}/bookmark`)
+      .set('Authorization', `Bearer ${createJWT(seeder.users[0])}`)
+      .send(data);
+
+    // then
+    expect(res.status).toBe(expectedError.status);
+    expect(res.body.message).toBe(expectedError.message);
+    expect(res.body.code).toBe(expectedError.code);
+    done();
+  });
+  it('빈 title로 생성하려는 경우', async done => {
+    // given
+    const taskId = seeder.tasks[0].id;
+    const data = { url: 'https://www.naver.com', title: '' };
+    const expectedError = customError.INVALID_INPUT_ERROR('title');
+
+    // when
+    const res = await request(app)
+      .post(`/api/task/${taskId}/bookmark`)
+      .set('Authorization', `Bearer ${createJWT(seeder.users[0])}`)
+      .send(data);
+
+    // then
+    expect(res.status).toBe(expectedError.status);
+    expect(res.body.message).toBe(expectedError.message);
+    expect(res.body.code).toBe(expectedError.code);
+    done();
+  });
+  it('taskId가 uuid가 아닌 경우', async done => {
+    // given
+    const taskId = 'invalid taskId';
+    const data = { url: 'https://www.naver.com', title: '네이버' };
+    const expectedError = customError.INVALID_INPUT_ERROR('taskId');
+
+    // when
+    const res = await request(app)
+      .post(`/api/task/${taskId}/bookmark`)
+      .set('Authorization', `Bearer ${createJWT(seeder.users[0])}`)
+      .send(data);
+
+    // then
+    expect(res.status).toBe(expectedError.status);
+    expect(res.body.message).toBe(expectedError.message);
+    expect(res.body.code).toBe(expectedError.code);
+    done();
+  });
+  it('task가 존재하지 않는 경우', async done => {
+    // given
+    const taskId = seeder.sections[0].id;
+    const data = { url: 'https://www.naver.com', title: '네이버' };
+    const expectedError = customError.NOT_FOUND_ERROR('task');
+
+    // when
+    const res = await request(app)
+      .post(`/api/task/${taskId}/bookmark`)
+      .set('Authorization', `Bearer ${createJWT(seeder.users[0])}`)
+      .send(data);
+
+    // then
+    expect(res.status).toBe(expectedError.status);
+    expect(res.body.message).toBe(expectedError.message);
+    expect(res.body.code).toBe(expectedError.code);
+    done();
+  });
+  it('자신의 task가 아닌 경우', async done => {
+    // given
+    const taskId = seeder.tasks[0].id;
+    const data = { url: 'https://www.naver.com', title: 'hi' };
+    const expectedError = customError.FORBIDDEN_ERROR('task');
+
+    // when
+    const res = await request(app)
+      .post(`/api/task/${taskId}/bookmark`)
+      .set('Authorization', `Bearer ${createJWT(seeder.users[2])}`)
+      .send(data);
+
+    // then
+    expect(res.status).toBe(expectedError.status);
+    expect(res.body.message).toBe(expectedError.message);
+    expect(res.body.code).toBe(expectedError.code);
+    done();
+  });
+});
 
 // describe('delete bookmark', () => {
 //   it('bookmark 삭제 성공', async done => {
