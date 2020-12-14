80f6e0b7026beec12d0829738858d3c477d83db1
Jin Young Park <jy2045@sogang.ac.kr>
2020-12-15 03:43:43 +0900

    [FE] FEAT: bookmark myAxios API 구현

---
+++ client/src/api/bookmark.js
@@ -0,0 +1,15 @@
+import myAxios from "./myAxios";
+
+const bookmarkAPI = {
+  getAllBookmarks(taskId) {
+    return myAxios.get(`/task/${taskId}/bookmark`);
+  },
+  createBookmark(taskId, data) {
+    return myAxios.post(`/task/${taskId}/bookmark`, data);
+  },
+  deleteBookmark(taskId, bookmarkId) {
+    return myAxios.delete(`/task/${taskId}/comment/${bookmarkId}`);
+  },
+};
+
+export default bookmarkAPI;
