c56e83f68a20853da83460df0f936ac487af5a2d
Jin Young Park <jy2045@sogang.ac.kr>
2020-12-15 03:54:07 +0900

    [FE] FEAT: bookmark store 구현

--- client/src/store/bookmark.js
+++ client/src/store/bookmark.js
@@ -1,16 +1,49 @@
-// import commentAPI from "@/api/comment";
+import bookmarkAPI from "@/api/bookmark";
 
 const state = {
-  bookmarks: [],
+  bookmarkMap: {},
 };
 
 const getters = {};
 
 const mutations = {
-  SET_BOOKMARKS: (state, bookmarks) => (state.bookmarks = bookmarks),
+  SET_BOOKMARKS: (state, { bookmarks, taskId }) => {
+    const newBookmarks = {};
+    newBookmarks[taskId] = bookmarks;
+    state.bookmarkMap = { ...state.bookmarkMap, ...newBookmarks };
+  },
 };
 
-const actions = {};
+const actions = {
+  async fetchBookmarks({ commit }, taskId) {
+    try {
+      const {
+        data: { bookmarks },
+      } = await bookmarkAPI.getAllBookmarks(taskId);
+      commit("SET_BOOKMARKS", { bookmarks, taskId });
+    } catch (err) {
+      commit("SET_ERROR_ALERT", err.response);
+    }
+  },
+  async addBookmark({ commit, dispatch }, bookmark) {
+    try {
+      await bookmarkAPI.createBookmark(bookmark);
+      await dispatch("fetchBookmarks", bookmark.taskId);
+      commit("SET_SUCCESS_ALERT", "북마크가 생성되었습니다.");
+    } catch (err) {
+      commit("SET_ERROR_ALERT", err.response);
+    }
+  },
+  async deleteBookmark({ commit, dispatch }, bookmark) {
+    try {
+      await bookmarkAPI.deleteBookmark(bookmark);
+      await dispatch("fetchComments", bookmark.taskId);
+      commit("SET_SUCCESS_ALERT", "북마크가 삭제되었습니다.");
+    } catch (err) {
+      commit("SET_ERROR_ALERT", err.response);
+    }
+  },
+};
 
 export default {
   state,
