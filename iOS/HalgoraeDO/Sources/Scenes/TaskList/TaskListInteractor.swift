56558d5c61d078e4f925d243508f4673a16df6db
kernel <tkddbs0901@khu.ac.kr>
2020-12-17 17:25:08 +0900

    [iOS] feat: board뷰 interactor 추가

--- iOS/HalgoraeDO/Sources/Scenes/TaskList/TaskListInteractor.swift
+++ iOS/HalgoraeDO/Sources/Scenes/TaskList/TaskListInteractor.swift
@@ -14,7 +14,8 @@ protocol TaskListBusinessLogic {
     func updateCompleteAll(request: TaskListModels.FinishTask.Request, projectId: String)
     func createTask(request: TaskListModels.CreateTask.Request)
     func createSection(request: TaskListModels.CreateSection.Request)
-    func dragDropHelper(requset: TaskListModels.DragDropTask.DragDropRequest) 
+    func dragDropForList(requset: TaskListModels.DragDropTask.RequestForList)
+    func dragDropForBoard(requset: TaskListModels.DragDropTask.RequestForBoard)
     func fetchDragDrop(request: TaskListModels.MoveTask.Request)
 }
 
@@ -60,11 +61,7 @@ extension TaskListInteractor: TaskListBusinessLogic {
             worker.request(endPoint: TaskEndPoint.taskUpdate(id: viewModel.id, task: data)) { (project: Response<String>?) in }
             
         }
-        
         let tasks = taskList.tasks(taskVMs: viewModels)
-//        for task in tasks {
-//            task.isDone = !task.isDone
-//        }
         presenter.presentFinshChanged(response: .init(tasks: tasks))
     }
     
@@ -110,8 +107,14 @@ extension TaskListInteractor: TaskListBusinessLogic {
         }
     }
     
-    func dragDropHelper(requset: TaskListModels.DragDropTask.DragDropRequest) {
-       let apiEndPoint = dragDropInteractor.dropHelper(projectId: requset.projectId, childCheck: requset.childCheck, sourceIndexPath: requset.sourceIndexPath, destinationIndexPath: requset.destinationIndexPath, dataSource: requset.dataSource, destinationCell: requset.destinationCell)
+    func dragDropForList(requset: TaskListModels.DragDropTask.RequestForList) {
+       let apiEndPoint = dragDropInteractor.dropHelperForList(projectId: requset.projectId, childCheck: requset.childCheck, sourceIndexPath: requset.sourceIndexPath, destinationIndexPath: requset.destinationIndexPath, dataSource: requset.dataSource, destinationCell: requset.destinationCell)
+        guard let endPoint = apiEndPoint else { return }
+        fetchDragDrop(request: endPoint)
+    }
+    
+    func dragDropForBoard(requset: TaskListModels.DragDropTask.RequestForBoard) {
+        let apiEndPoint = dragDropInteractor.dropHelperForBoard(projectId: requset.projectId, sectionViewModel: requset.sectionViewModel, sourceSection: requset.sourceSection, destinationSection: requset.destinationSection, sourceTask: requset.sourceTask, destinationTask: requset.destinationTask)
         guard let endPoint = apiEndPoint else { return }
         fetchDragDrop(request: endPoint)
     }
@@ -119,19 +122,16 @@ extension TaskListInteractor: TaskListBusinessLogic {
     func fetchDragDrop(request: TaskListModels.MoveTask.Request) {
         guard let moveSectionData = request.taskMoveSection.encodeData,
               let taskMoveData = request.taskMoveFields.encodeData
-        else {
-            return
-        }
+        else { return }
         
         guard let projectId = request.projectId else {//task 하위에
             guard let parentTaskId = request.parentTaskId else { return }
             worker.requestPatchAndPatch(patch: TaskEndPoint.taskUpdate(id: request.taskId, task: moveSectionData), endPoint: TaskEndPoint.moveIntoTask(taskId: parentTaskId, request: taskMoveData)){ (response: Response<String>?) in
             }
-           return
+            return
         }
         
-        //섹션의 root items모두 전송
         worker.requestPatchAndPatch(patch: TaskEndPoint.taskUpdate(id: request.taskId, task: moveSectionData), endPoint: TaskEndPoint.moveIntoSection(projectId: projectId, sectionId: request.sectionId, request: taskMoveData)){ (response: Response<String>?) in
-        }
+        }//섹션의 root items모두 전송
     }
 }
