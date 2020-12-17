53a7ae561a5770bcd1c02b101716ce609978b549
Jin Young Park <jy2045@sogang.ac.kr>
2020-12-18 02:41:23 +0900

    [FE] REFACTOR: axios api 호출시 loading spinner 동작 세분화
    - get method일때만 호출.
    - user/me, project는 초기 렌더링시 호출되므로 end 이벤트를 발생시키지
      않는다.(spinner가 계속 돌기 위해서)

--- client/src/api/myAxios.js
+++ client/src/api/myAxios.js
@@ -9,7 +9,27 @@ const myAxios = axios.create({
 
 myAxios.interceptors.request.use((config) => {
   config.headers.Authorization = "Bearer " + localStorage.getItem("token");
+
+  if (config.method === "get" && config.url !== "/task") {
+    bus.$emit("start:spinner");
+  }
   return config;
 });
+
+myAxios.interceptors.response.use(
+  (res) => {
+    if (res.config.url === "/user/me" || res.config.url === "/project") {
+      return res;
+    }
+    if (res.config.method === "get") {
+      bus.$emit("end:spinner");
+    }
+    return res;
+  },
+  (error) => {
+    bus.$emit("end:spinner");
+    return Promise.reject(error);
+  }
+);
 
 export default myAxios;
