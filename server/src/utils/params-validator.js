a8e758a703b61a2076c4879564c899abcf23ff0f
dimpl@JinYoung <dimple0416@naver.com>
2020-12-17 21:08:02 +0900

    [BE] REFACTOR: params 데코레이터 위치 수정

--- server/src/utils/params-validator.js
+++ server/src/utils/params-validator.js
@@ -3,23 +3,23 @@ const errorMessage = require('@utils/custom-error').message;
 
 class ParamsValidator {
   @ValidateIf(o => typeof o.projectId !== 'undefined')
-  @IsString({ message: errorMessage.TYPE_ERROR('projectId') })
   @IsUUID('4', { message: errorMessage.INVALID_INPUT_ERROR('projectId') })
+  @IsString({ message: errorMessage.TYPE_ERROR('projectId') })
   projectId;
 
   @ValidateIf(o => typeof o.sectionId !== 'undefined')
-  @IsString({ message: errorMessage.TYPE_ERROR('sectionId') })
   @IsUUID('4', { message: errorMessage.INVALID_INPUT_ERROR('sectionId') })
+  @IsString({ message: errorMessage.TYPE_ERROR('sectionId') })
   sectionId;
 
   @ValidateIf(o => typeof o.taskId !== 'undefined')
-  @IsString({ message: errorMessage.TYPE_ERROR('taskId') })
   @IsUUID('4', { message: errorMessage.INVALID_INPUT_ERROR('taskId') })
+  @IsString({ message: errorMessage.TYPE_ERROR('taskId') })
   taskId;
 
   @ValidateIf(o => typeof o.commentId !== 'undefined')
-  @IsString({ message: errorMessage.TYPE_ERROR('commentId') })
   @IsUUID('4', { message: errorMessage.INVALID_INPUT_ERROR('commentId') })
+  @IsString({ message: errorMessage.TYPE_ERROR('commentId') })
   commentId;
 
   @ValidateIf(o => typeof o.bookmarkId !== 'undefined')
