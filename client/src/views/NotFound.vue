ad281d083ee3eeb4bdfd13da6a84b0c89ae598d9
Jin Young Park <jy2045@sogang.ac.kr>
2020-12-18 05:23:25 +0900

    [FE] FEAT: NOTFOUND 라우터 추가

---
+++ client/src/views/NotFound.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-layout fill-height d-flex align-center justify-center flex-column>
+      <div class="white d-flex flex-column justify-space-around align-center py-10">
+        <img width="400" src="../assets/halgoraedo.png" />
+        <div class="text-h1">NOT FOUND</div>
+      </div>
+      <alert></alert>
+    </v-layout>
+  </v-app>
+</template>
+
+<script>
+import SpinnerMixin from "@/mixins/SpinnerMixins";
+
+export default {
+  mixins: [SpinnerMixin],
+};
+</script>
