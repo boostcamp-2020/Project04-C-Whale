fd3fc6d4e2353e686ef3d25cfbd681750e9e1355
woongs <ccw1021@naver.com>
2020-12-17 14:09:28 +0900

    [iOS] fix: 사용하지 않는 Response 제거

--- iOS/HalgoraeDO/Sources/Service/DataRequest.swift
+++ iOS/HalgoraeDO/Sources/Service/DataRequest.swift
@@ -13,10 +13,6 @@ class DataRequest {
     
     enum NetworkResponse: String {
         case success
-        case badURL = "Bad URL"
-        case authenticationError = "You need to be authenticated first."
-        case badRequest = "Bad request"
-        case outdated = "The url you requested is outdated."
         case failed = "Network request failed."
         case noData = "Response returned with no data to decode."
         case unableToDecode = "We could not decode the response."
