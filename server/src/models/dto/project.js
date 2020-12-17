bb2b9a6ec32f92d4b1c448333cc1144d800cf634
dimpl@JinYoung <dimple0416@naver.com>
2020-12-18 00:54:50 +0900

    [BE] REFACTOR: project dto 수정

--- server/src/models/dto/project.js
+++ server/src/models/dto/project.js
@@ -1,25 +1,21 @@
-const { IsString, ValidateIf, IsHexColor, MinLength, IsBoolean } = require('class-validator');
-const errorMessage = require('@utils/custom-error').message;
+const { IsString, IsHexColor, MinLength, IsBoolean, IsEmpty } = require('class-validator');
 
 class ProjectDto {
+  @IsEmpty()
   id;
 
-  @ValidateIf(o => !!o.title)
-  @MinLength(1, { groups: ['create'], message: errorMessage.INVALID_INPUT_ERROR() })
-  @IsString({ groups: ['create'], message: errorMessage.TYPE_ERROR() })
+  @MinLength(1)
+  @IsString()
   title;
 
-  @ValidateIf(o => !!o.color)
-  @IsHexColor({ groups: ['created'], message: errorMessage.INVALID_INPUT_ERROR() })
-  @IsString({ groups: ['create'], message: errorMessage.TYPE_ERROR() })
+  @IsHexColor()
+  @IsString()
   color;
 
-  @ValidateIf(o => !!o.isList)
-  @IsBoolean({ groups: ['create'], message: errorMessage.TYPE_ERROR() })
+  @IsBoolean()
   isList;
 
-  @ValidateIf(o => !!o.isFavorite)
-  @IsBoolean({ message: errorMessage.TYPE_ERROR() })
+  @IsBoolean()
   isFavorite;
 }
 
