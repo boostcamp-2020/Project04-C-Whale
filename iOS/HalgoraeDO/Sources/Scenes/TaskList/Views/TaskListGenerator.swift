198ccc90aaf3621bc86a422bd03c4c8ddc645d1f
woongs <ccw1021@naver.com>
2020-12-19 18:23:50 +0900

    [iOS] feat: TaskListGenerator 생성

--- iOS/HalgoraeDO/Sources/Scenes/TaskList/Views/TaskListGenerator.swift
+++ iOS/HalgoraeDO/Sources/Scenes/TaskList/Views/TaskListGenerator.swift
@@ -5,4 +5,25 @@
 //  Created by woong on 2020/12/19.
 //
 
-import Foundation
+import UIKit
+
+class TaskListGenerator {
+    
+    typealias TaskVM = TaskListModels.TaskVM
+    
+    var registration = CellRegistration()
+}
+
+extension TaskListGenerator {
+    struct CellRegistration {
+        func normal(doneHandler: ((TaskCollectionViewListCell?, TaskListModels.TaskVM) -> Void)?) -> UICollectionView.CellRegistration<TaskCollectionViewListCell, TaskVM> {
+            return UICollectionView.CellRegistration<TaskCollectionViewListCell, TaskVM> { (cell, _: IndexPath, taskItem) in
+                cell.indentationWidth = 25
+                cell.taskViewModel = taskItem
+                cell.doneHandler = doneHandler
+                let disclosureOptions = UICellAccessory.OutlineDisclosureOptions(style: .automatic)
+                cell.accessories = taskItem.subItems.isEmpty ? [] : [.outlineDisclosure(options: disclosureOptions)]
+            }
+        }
+    }
+}
