9e775ab91f83da6ee694e897522a0139450dc78d
Jin Young Park <jy2045@sogang.ac.kr>
2020-12-20 13:09:25 +0900

    [FE] FIX: 네아로 막아두기. 로그인 화면 색상 변경

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,14 +1,15 @@
 <template>
   <v-app>
-    <v-layout class="whaleGreen" fill-height d-flex align-center justify-center flex-column>
-      <div class="login-layout white d-flex flex-column justify-space-around align-center py-10">
+    <v-layout class="grey lighten-5" fill-height d-flex align-center justify-center flex-column>
+      <div class="login-layout d-flex flex-column justify-space-around align-center py-10">
         <img width="200" src="../assets/halgoraedo.png" />
-        <a :href="OAuthURL">
+        <!-- <a :href="OAuthURL">
           <img width="200" src="../assets/naverlogin.png" />
-        </a>
+        </a> -->
         <a :href="OAuthGoogleURL">
           <img width="200" src="../assets/googlelogin.png" />
         </a>
+        <p>네이버 로그인은 준비중입니다</p>
       </div>
       <alert></alert>
     </v-layout>
@@ -37,5 +38,7 @@ export default {
   width: 360px;
   height: 360px;
   border-radius: 0.5rem;
+  background-color: white;
+  border: 1px solid #dbdbdb !important;
 }
 </style>
