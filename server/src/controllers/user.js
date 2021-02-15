7c484741d9a3e6e66ac1c2483bf6991d57259efd
shkillo <kyle.hwan.k@gmail.com>
2021-02-15 20:33:51 +0900

    TEST: ci cd test

--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -32,7 +32,7 @@ const googleLogin = asyncTryCatch(async (req, res) => {
 
 const getOwnInfo = asyncTryCatch(async (req, res) => {
   const { user } = req;
-  responseHandler(res, 200, { user, test: false });
+  responseHandler(res, 200, { user });
 });
 
 module.exports = { naverLogin, googleLogin, getOwnInfo };
